075232f3d375b8e7e396b70a9739730bf31185a4
Cathyprime <magdajaroszynska47@gmail.com>
2023-02-14 11:13:15 +0100

    one file to choose colorscheme instead in the respective color file

--- lua/colorschemes/sonokai.lua
+++ lua/colorschemes/sonokai.lua
@@ -3,12 +3,6 @@ vim.cmd("let g:sonokai_transparent_background=1")
 
 return {
     "sainnhe/sonokai",
-    -- {
-    --     "LazyVim/LazyVim",
-    --     opts = {
-    --         colorscheme = "sonokai",
-    --     },
-    -- },
     require("notify").setup({
         background_colour = "#000000",
     }),
