2279c373c677e59acfce047e2ae561420c818a51
yoolayn <jaroszynskijulian@gmail.com>
2023-02-16 23:39:42 +0100

    more norg integrations

--- lua/plugins/norg.lua
+++ lua/plugins/norg.lua
@@ -7,15 +7,20 @@ return {
         load = {
             ["core.defaults"] = {}, -- Loads default behaviour
             ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+            ["core.integrations.telescope"] = {}, -- telescope integration to add links and search headings
+            ["core.integrations.treesitter"] = {}, -- treesitter integration
+            ["core.integrations.nvim-cmp"] = {}, -- autocompletion
             ["core.norg.dirman"] = { -- Manages Neorg workspaces
                 config = {
                     workspaces = {
                         notes = "~/notes",
                         testing = "~/notes/testing",
+                        documentation = "~/notes/documentation",
                     },
                 },
             },
+            ["core.norg.dirman.utils"] = {}, -- utilities for neorg
         },
     },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    dependencies = { { "nvim-lua/plenary.nvim", "nvim-neorg/neorg-telescope" } },
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,5 +7,8 @@ return {
             ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
             ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
         })
+        opts.sources = vim.tbl_extend("force", opts.sources, {
+            { name = "neorg" },
+        })
     end,
 }
