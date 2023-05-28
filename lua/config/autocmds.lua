1ef895afcfed3cb9fb72c9a5b5880b18b666faca
Yoolayn <jaroszynskijulian@gmail.com>
2023-05-28 11:34:46 +0200

    neotree navic + autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+    return vim.api.nvim_create_augroup("yoolayn_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    group = augroup("highlight_yank"),
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual" })
+    end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+    group = augroup("resize_splits"),
+    callback = function()
+        vim.cmd("tabdo wincmd =")
+    end,
+})
