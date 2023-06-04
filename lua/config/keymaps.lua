edbea0ce38cac627e140d092ad3a488aab27a0bd
Yoolayn <jaroszynskijulian@gmail.com>
2023-06-04 10:52:20 +0200

    stuffs
    moved from keybinds to move lines to mini.move
    renamed animate to mini.animate to see which mini plugins i have installed
    created keybinds to make it easier to interact with system keyboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
 local keymap = vim.keymap.set
+local wk = require("which-key").register
 
 -- leader
 vim.g.mapleader = " "
@@ -7,17 +8,20 @@ vim.g.mapleader = " "
 keymap("n", "gh", "<cmd>Alpha<CR>", { silent = true, desc = "Dashboard" })
 
 -- system keyboard
-require("which-key").register({
+wk({
     K = {
         name = "Keyboard",
+        y = { '"+y', "yank to system keyboard" },
+        Y = { '"+Y', "yank line to system keyboard" },
+        d = { '"+d', "cut to system keyboard" },
+        D = { '"+D', "cut line to system keyboard" },
+        p = { '"+p', "paste from system keyboard" },
+        P = { '"+P', "paste from system keyboard before" },
     },
-}, { prefix = "<leader>" })
-keymap("n", "<leader>Ky", '"+y', { silent = true, desc = "copy to system keyboard" })
-keymap("n", "<leader>Ky", '"+Y', { silent = true, desc = "copy line to system keyboard" })
-keymap("n", "<leader>Kd", '"+d', { silent = true, desc = "cut to system keyboard" })
-keymap("n", "<leader>KD", '"+D', { silent = true, desc = "cut line to system keyboard" })
-keymap("n", "<leader>Kp", '"+p', { silent = true, desc = "paste from system keyboard" })
-keymap("n", "<leader>KP", '"+P', { silent = true, desc = "paste from system keyboard before" })
+}, {
+    prefix = "<leader>",
+    mode = { "n", "v" },
+})
 
 -- telescope
 keymap("n", "<Leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" })
@@ -26,28 +30,13 @@ keymap("n", "<Leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Help Tags" }
 -- undo tree
 keymap("n", "<F5>", "<cmd>UndotreeToggle<CR>", { noremap = true, silent = true })
 
--- Switch lines VS**** style
-keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
-keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
-keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
-keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
-
 -- nvterm
-keymap("n", "<A-h>", function()
+keymap({ "n", "t" }, "<A-h>", function()
     require("nvterm.terminal").toggle("horizontal")
 end, { noremap = true, silent = true })
-keymap("n", "<A-v>", function()
+keymap({ "n", "t" }, "<A-v>", function()
     require("nvterm.terminal").toggle("vertical")
 end, { noremap = true, silent = true })
-keymap("n", "<A-i>", function()
-    require("nvterm.terminal").toggle("float")
-end, { noremap = true, silent = true })
-keymap("t", "<A-h>", function()
-    require("nvterm.terminal").toggle("horizontal")
-end, { noremap = true, silent = true })
-keymap("t", "<A-v>", function()
-    require("nvterm.terminal").toggle("vertical")
-end, { noremap = true, silent = true })
-keymap("t", "<A-i>", function()
+keymap({ "n", "t" }, "<A-i>", function()
     require("nvterm.terminal").toggle("float")
 end, { noremap = true, silent = true })
