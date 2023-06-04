a53c4e83620fe68bc6eaa40eb0e5007ed9ee2b13
Cathyprime <magdajaroszynska47@gmail.com>
2023-06-04 18:05:46 +0200

    nvterm deleted + scala fixes

--- lua/plugins/scala.lua
+++ lua/plugins/scala.lua
@@ -5,6 +5,7 @@ vim.api.nvim_create_autocmd("FileType", {
         require("metals").initialize_or_attach({})
         metals_config = require("metals").bare_config()
         metals_config.settings = {
+            showImplicitArguments = true,
             serverVersion = "latest.snapshot",
         }
         metals_config.init_options.statusBarProvider = "on"
