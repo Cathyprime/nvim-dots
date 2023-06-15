16f398e5905d8edd8720da88efadd53eb225a3ca
Cathyprime <magdajaroszynska47@gmail.com>
2023-06-15 22:53:02 +0200

    harpoon file

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -27,6 +27,19 @@ return {
             desc = "Add File",
         },
         {
+            "<leader>hg",
+            function()
+                local result = vim.fn.input({ prompt = "Enter mark number: " })
+                local number = tonumber(result)
+                if number ~= nil then
+                    require("harpoon.ui").nav_file(number)
+                else
+                    vim.api.nvim_echo({ { "Enter a number!", "Normal" } }, true, {})
+                end
+            end,
+            desc = "go to mark {arg}",
+        },
+        {
             "<leader>hn",
             function()
                 require("harpoon.ui").nav_next()
