b38a702d7057d157232d8b8bfcec1e8c9465c409
Cathyprime <magdajaroszynska47@gmail.com>
2023-06-24 12:05:01 +0200

    flash
    +flash
    disabled leap, flash will take over this function
    
    disabled operator support for which-key, it was annoying and flash remote wasn't working because of it

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/which-key.nvim",
+    config = function()
+        require("which-key").setup({
+            plugins = {
+                presets = {
+                    operators = false,
+                },
+            },
+        })
+    end,
+}
