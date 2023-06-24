593e7f730fd0cdeafc68e9b0c6f51426dc2434f6
Yoolayn <jaroszynskijulian@gmail.com>
2023-06-24 12:05:01 +0200

    flash
    +flash
    disabled leap, flash will take over this function
    
    disabled operator support for which-key, it was annoying and flash remote wasn't working because of it

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
     { "echasnovski/mini.comment", enabled = true },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "ggandor/flit.nvim", enabled = false },
+    { "ggandor/leap.nvim", enabled = false },
 }
