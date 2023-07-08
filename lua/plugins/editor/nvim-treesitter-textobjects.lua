c1e0d8a577448d57891ec7a18c0807446eb71be2
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-08 23:36:04 +0200

    fix: more lazy loading
    fix: added missing changes

--- lua/plugins/editor/nvim-treesitter-textobjects.lua
+++ lua/plugins/editor/nvim-treesitter-textobjects.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
+	event = { "BufReadPost", "BufNewFile" },
 	init = function()
 		require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
 		load_textobjects = true
