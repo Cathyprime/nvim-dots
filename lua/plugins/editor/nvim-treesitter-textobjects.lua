fcf7d7d6b22bd569688d852fc9998bdef4ef414c
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-09 00:46:23 +0200

    refractor: all treesitter plugins are in one file

--- lua/plugins/editor/nvim-treesitter-textobjects.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	event = { "BufReadPost", "BufNewFile" },
-	init = function()
-		require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-		load_textobjects = true
-	end,
-}
