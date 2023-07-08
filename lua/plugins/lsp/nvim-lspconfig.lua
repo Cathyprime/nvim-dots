b283369f2ca06cb58082fa63e2e2759570dbb522
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-08 21:51:41 +0200

    feat(lang): rust support
    lsp: rust analyzer, taplo
    plugins: rust-tools.nvim, crates.nvim
    treesitter: rust, ron, toml

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -89,6 +89,51 @@ return {
 					clangdFileStatus = true,
 				},
 			},
+			rust_analyzer = {
+				keys = {
+					{ "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+					{ "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+					{ "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+				},
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+							loadOutDirsFromCheck = true,
+							runBuildScripts = true,
+						},
+						-- Add clippy lints for Rust.
+						checkOnSave = {
+							allFeatures = true,
+							command = "clippy",
+							extraArgs = { "--no-deps" },
+						},
+						procMacro = {
+							enable = true,
+							ignored = {
+								["async-trait"] = { "async_trait" },
+								["napi-derive"] = { "napi" },
+								["async-recursion"] = { "async_recursion" },
+							},
+						},
+					},
+				},
+			},
+			taplo = {
+				keys = {
+					{
+						"K",
+						function()
+							if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+								require("crates").show_popup()
+							else
+								vim.lsp.buf.hover()
+							end
+						end,
+						desc = "Show Crate Documentation",
+					},
+				},
+			},
 		},
 		setup = {
 			clangd = function(_, opts)
@@ -96,6 +141,11 @@ return {
 				require("clangd_extensions").setup(
 					vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts })
 				)
+				return true
+			end,
+			rust_analyzer = function(_, opts)
+				local rust_tools_opts = require("yoolayn.util.funcs").opts("rust-tools.nvim")
+				require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
 				return true
 			end,
 		},
