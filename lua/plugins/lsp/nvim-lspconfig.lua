dfd8df04bfbf224f99901e59593d334408fc7795
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-08 21:51:05 +0200

    feat(lang): C and CPP support

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -55,6 +55,49 @@ return {
 					},
 				},
 			},
+			clangd = {
+				keys = {
+					{
+						"<leader>cR",
+						"<cmd>ClangdSwitchSourceHeader<cr>",
+						desc = "Switch Source/Header (C/C++)",
+					},
+				},
+				root_dir = function(...)
+					return require("lspconfig.util").root_pattern(
+						"compile_commands.json",
+						"compile_flags.txt",
+						"configure.ac",
+						".git"
+					)(...)
+				end,
+				capabilities = {
+					offsetEncoding = { "utf-16" },
+				},
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style=llvm",
+				},
+				init_options = {
+					usePlaceholders = true,
+					completeUnimported = true,
+					clangdFileStatus = true,
+				},
+			},
+		},
+		setup = {
+			clangd = function(_, opts)
+				local clangd_ext_opts = require("yoolayn.util.funcs").opts("clangd_extensions.nvim")
+				require("clangd_extensions").setup(
+					vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts })
+				)
+				return true
+			end,
 		},
 	},
 	---@param opts PluginLspOpts
