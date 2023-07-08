3f998a254610e9e8db42ae2f0c6062493378f0db
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-08 23:39:05 +0200

    fix: move configuration for languages to their respective file

--- lua/plugins/coding/langs/rust.lua
+++ lua/plugins/coding/langs/rust.lua
@@ -1,22 +1,8 @@
 return {
-	"simrat39/rust-tools.nvim",
-	lazy = true,
-	opts = function()
-		local ok, mason_registry = pcall(require, "mason-registry")
-		local adapter ---@type any
-		if ok then
-			-- rust tools configuration for debugging support
-			local codelldb = mason_registry.get_package("codelldb")
-			local extension_path = codelldb:get_install_path() .. "/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-				or extension_path .. "lldb/lib/liblldb.so"
-			adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-		end
-		return {
-			dap = {
-				adapter = adapter,
-			},
+	{
+		"simrat39/rust-tools.nvim",
+		lazy = true,
+		opts = {
 			tools = {
 				on_initialized = function()
 					vim.cmd([[
@@ -28,7 +14,78 @@ return {
                 ]])
 				end,
 			},
-		}
-	end,
-	config = function() end,
+		},
+		config = function() end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				vim.list_extend(opts.ensure_installed, { "ron", "rust", "toml" })
+			end
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				-- Ensure mason installs the server
+				rust_analyzer = {
+					keys = {
+						{ "K", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
+						{ "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+						{ "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
+					},
+					settings = {
+						["rust-analyzer"] = {
+							lens = {
+								enable = false,
+							},
+							cargo = {
+								allFeatures = true,
+								loadOutDirsFromCheck = true,
+								runBuildScripts = true,
+							},
+							-- Add clippy lints for Rust.
+							checkOnSave = {
+								allFeatures = true,
+								command = "clippy",
+								extraArgs = { "--no-deps" },
+							},
+							procMacro = {
+								enable = true,
+								ignored = {
+									["async-trait"] = { "async_trait" },
+									["napi-derive"] = { "napi" },
+									["async-recursion"] = { "async_recursion" },
+								},
+							},
+						},
+					},
+				},
+				taplo = {
+					keys = {
+						{
+							"K",
+							function()
+								if vim.fn.expand("%:t") == "Cargo.toml" and require("crates").popup_available() then
+									require("crates").show_popup()
+								else
+									vim.lsp.buf.hover()
+								end
+							end,
+							desc = "Show Crate Documentation",
+						},
+					},
+				},
+			},
+			setup = {
+				rust_analyzer = function(_, opts)
+					local rust_tools_opts = require("yoolayn.util.funcs").opts("rust-tools.nvim")
+					require("rust-tools").setup(vim.tbl_deep_extend("force", rust_tools_opts or {}, { server = opts }))
+					return true
+				end,
+			},
+		},
+	},
 }
--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -7,9 +7,6 @@ return {
 		{
 			"nvim-treesitter/nvim-treesitter-textobjects",
 			init = function()
-				-- disable rtp plugin, as we only need its queries for mini.ai
-				-- In case other textobject modules are enabled, we will load them
-				-- once nvim-treesitter is loaded
 				require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
 				load_textobjects = true
 			end,
@@ -42,11 +39,6 @@ return {
 			"vim",
 			"vimdoc",
 			"yaml",
-			"ron",
-			"rust",
-			"toml",
-			"c",
-			"cpp",
 		},
 		incremental_selection = {
 			enable = true,
