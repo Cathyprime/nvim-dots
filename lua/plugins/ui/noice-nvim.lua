ca8c9d023589daeeaf8d5c3ba87918ed8064820d
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-09 00:46:50 +0200

    fix: shorter noice keybinds, and group named using which key

--- lua/plugins/ui/noice-nvim.lua
+++ lua/plugins/ui/noice-nvim.lua
@@ -33,14 +33,21 @@ return {
     -- stylua: ignore
     keys = {
         { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
-        { "<leader>snl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
-        { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
-        { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
-        { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+        { "<leader>nl", function() require("noice").cmd("last") end, desc = "Noice Last Message" },
+        { "<leader>nh", function() require("noice").cmd("history") end, desc = "Noice History" },
+        { "<leader>na", function() require("noice").cmd("all") end, desc = "Noice All" },
+        { "<leader>nd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
         { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
         { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
     },
 	config = function()
+		if require("yoolayn.util.funcs").has("which-key.nvim") then
+			require("which-key").register({
+				["<leader>n"] = {
+					name = "+notifications",
+				},
+			})
+		end
 		require("noice").setup({
 			cmdline = {
 				view = "cmdline",
