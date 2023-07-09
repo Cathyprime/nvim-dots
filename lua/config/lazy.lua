47782881c869eca95d56ff42f1218c3d199290ec
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-09 02:52:40 +0200

    refractor: move test core around

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,11 +12,11 @@ require("lazy").setup({
         -- import any extras modules here
         { import = "lazyvim.plugins.extras.editor.flash" },
         { import = "lazyvim.plugins.extras.dap.core" },
+        { import = "lazyvim.plugins.extras.test.core" },
         { import = "lazyvim.plugins.extras.dap.nlua" },
         { import = "lazyvim.plugins.extras.lang.rust" },
         { import = "lazyvim.plugins.extras.lang.typescript" },
         { import = "lazyvim.plugins.extras.lang.json" },
-        { import = "lazyvim.plugins.extras.test.core" },
         { import = "lazyvim.plugins.extras.linting.eslint" },
         { import = "lazyvim.plugins.extras.ui.mini-animate" },
         -- import/override with your plugins
