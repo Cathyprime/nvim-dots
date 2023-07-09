614e87df63894d203d135e02d2b8e52862775d1e
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-09 02:26:21 +0200

    refractor: disable notify

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,6 @@
 return {
     { "echasnovski/mini.comment", enabled = true },
+    { "rcarriga/nvim-notify", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "ggandor/flit.nvim", enabled = false },
     { "ggandor/leap.nvim", enabled = false },
