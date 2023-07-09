922dd82bd13cc00e64d4e865a4d05776caef6128
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-09 02:31:43 +0200

    refractor: remove unnecessary comments

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,41 +2,8 @@ return {
     "folke/noice.nvim",
     config = function()
         require("noice").setup({
-            -- popout cmdline and search
-            -- views = {
-            --     cmdline_popup = {
-            --         position = {
-            --             row = 5,
-            --             col = "50%",
-            --         },
-            --         size = {
-            --             width = 60,
-            --             height = 0,
-            --         },
-            --     },
-            --     popupmenu = {
-            --         relative = "editor",
-            --         position = {
-            --             row = 8,
-            --             col = "50%",
-            --         },
-            --         size = {
-            --             width = 60,
-            --             height = 10,
-            --         },
-            --         border = {
-            --             style = "rounded",
-            --             padding = { 0, 1 },
-            --         },
-            --         win_options = {
-            --             winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-            --         },
-            --     },
-            -- },
             cmdline = {
-                -- classic cmdline
                 view = "cmdline",
-                -- classic search
                 format = {
                     search_down = {
                         view = "cmdline",
