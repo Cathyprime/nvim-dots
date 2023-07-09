73f54499cc8711b253e007370c39b36607a10e9d
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-09 02:32:20 +0200

    refractor: remove line not adding anything of value

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,4 @@
 return {
-    { "echasnovski/mini.comment", enabled = true },
     { "rcarriga/nvim-notify", enabled = false },
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "ggandor/flit.nvim", enabled = false },
