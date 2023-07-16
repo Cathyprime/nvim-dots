26dc5882c1a5d5478e27bfe7312dc483cea99066
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-16 21:48:24 +0200

    feat: colorschemes

---
+++ lua/yoolayn/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		'rose-pine/neovim',
+		name = 'rose-pine'
+	},
+	"navarasu/onedark.nvim",
+	"catppuccin/nvim",
+	"sainnhe/everforest",
+	"rebelot/kanagawa.nvim",
+	"cpea2506/one_monokai.nvim",
+	"EdenEast/nightfox.nvim",
+	"AlexvZyl/nordic.nvim",
+}
