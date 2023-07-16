5ed822bbe2d8dd4c1ca145b301d02fdcaaddd522
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-16 21:39:10 +0200

    BREAKING CHANGE: change fg -> fs and function
    grep_string changed since live_grep did the same thing
    added lsp_document_symbol for easier search in code

--- lua/yoolayn/plugins/files.lua
+++ lua/yoolayn/plugins/files.lua
@@ -12,12 +12,27 @@ return {
 			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "help tags" },
 			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "live grep" },
 			{
-				"<leader>fg",
+				"<leader>fs",
 				function()
-					require(telescope.builtin).grep_string({ search = vim.fn.input("Grep > ") })
+					require("telescope.builtin").lsp_document_symbols(
+					{
+						symbols = {
+							"Class",
+							"Function",
+							"Method",
+							"Constructor",
+							"Interface",
+							"Module",
+							"Struct",
+							"Trait",
+							"Field",
+							"Property",
+						}
+					})
 				end,
-				desc = "grep current file"
+				desc = "find symbols"
 			},
+
 			-- git
 			{ "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "find files" },
 			{ "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "status" },
