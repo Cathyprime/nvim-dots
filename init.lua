f823f8e99b7f59936d99c873f191b5442a031a21
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-18 20:10:14 +0200

    feat: autocommands

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
+require("yoolayn.options")
 require("yoolayn.keymaps")
 require("yoolayn.lazy")
 require("lsp-setup")
 require("yoolayn.colors")
-require("yoolayn.options")
+require("yoolayn.autocmds")
