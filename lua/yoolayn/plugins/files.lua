650b10aec63c4b3ab8d5e6e7b7d5722845dba8e2
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-18 16:14:00 +0200

    feat: spectre

--- lua/yoolayn/plugins/files.lua
+++ lua/yoolayn/plugins/files.lua
@@ -1,13 +1,24 @@
 return {
-	"stevearc/oil.nvim",
-	keys = {
-		{
-			"<leader>e",
-			function()
-				require("oil").open()
-			end,
-			desc = "open files",
+	{
+		"stevearc/oil.nvim",
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("oil").open()
+				end,
+				desc = "open files",
+			},
+		},
+		opts = {},
+	},
+	{
+		"nvim-pack/nvim-spectre",
+		cmd = "Spectre",
+		opts = { open_cmd = "noswapfile vnew" },
+		-- stylua: ignore
+		keys = {
+			{ "<leader>r", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
 		},
 	},
-	opts = {},
 }
