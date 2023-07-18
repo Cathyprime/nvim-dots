c6743628f6162adf054c8c0325bee2ab95f33675
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-18 21:49:25 +0200

    feat: material theme

--- lua/yoolayn/plugins/colorscheme.lua
+++ lua/yoolayn/plugins/colorscheme.lua
@@ -46,4 +46,33 @@ return {
 		"lunarvim/Onedarker.nvim",
 		lazy = true,
 	},
+	{
+		"marko-cerovac/material.nvim",
+		lazy = true,
+		opts = {
+			plugins = { -- Uncomment the plugins that you use to highlight them
+				-- Available plugins:
+				-- "dap",
+				-- "dashboard",
+				-- "gitsigns",
+				-- "hop",
+				"indent-blankline",
+				-- "lspsaga",
+				"mini",
+				-- "neogit",
+				-- "neorg",
+				"nvim-cmp",
+				-- "nvim-navic",
+				-- "nvim-tree",
+				"nvim-web-devicons",
+				-- "sneak",
+				"telescope",
+				"trouble",
+				-- "which-key",
+			},
+			disable = {
+				colored_cursor = true,
+			},
+		},
+	},
 }
