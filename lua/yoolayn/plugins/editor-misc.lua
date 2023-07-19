9f98dc4f7f7a1d8deb61f9bc7a4b037a995ab612
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-19 22:06:55 +0200

    feat: projects

--- lua/yoolayn/plugins/editor-misc.lua
+++ lua/yoolayn/plugins/editor-misc.lua
@@ -225,4 +225,19 @@ return {
 			})
 		end,
 	},
+	{
+		"ahmedkhalf/project.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("project_nvim").setup()
+			require("telescope").load_extension("projects")
+		end,
+		keys = {
+			{
+				"<leader>fp",
+				"<cmd>Telescope projects<cr>",
+				desc = "find project",
+			},
+		},
+	},
 }
