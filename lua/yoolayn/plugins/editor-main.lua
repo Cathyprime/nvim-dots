d9e78bcd0fdb2989df13aa99cb58b70482957d07
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-20 22:09:38 +0200

    style: configs

--- lua/yoolayn/plugins/editor-main.lua
+++ lua/yoolayn/plugins/editor-main.lua
@@ -1,136 +1,214 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					mappings = {
-						i = {
-							["<C-j>"] = function(...)
-								return require("telescope.actions").preview_scrolling_down(...)
-							end,
-							["<C-k>"] = function(...)
-								return require("telescope.actions").preview_scrolling_up(...)
-							end,
-						},
-					},
-				},
-			})
-		end,
-		keys = {
-			-- general
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "find files" },
-			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "find files" },
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "find Buffers" },
-			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "help tags" },
-			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "live grep" },
-			{
-				"<leader>fG",
-				function()
-					require("telescope.builtin").live_grep({ search_dirs = { vim.fn.expand("%:p") } })
-				end,
-				desc = "live grep current file",
-			},
-			{ "<leader>fk", "<cmd>Telescope keymaps<cr>", desc = "find keymaps" },
-			{ "<leader>fm", "<cmd>Telescope marks<cr>", desc = "find marks" },
-			{ "<leader>fM", "<cmd>Telescope man_pages<cr>", desc = "find manpages" },
-			{
-				"<leader>fs",
-				function()
-					require("telescope.builtin").lsp_document_symbols({
-						symbols = {
-							"Class",
-							"Function",
-							"Method",
-							"Constructor",
-							"Interface",
-							"Module",
-							"Struct",
-							"Trait",
-							"Field",
-							"Property",
-						},
-					})
-				end,
-				desc = "find symbols",
-			},
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-d>"] = function(...)
+                                return require("telescope.actions").preview_scrolling_down(
+                                    ...
+                                )
+                            end,
+                            ["<C-u>"] = function(...)
+                                return require("telescope.actions").preview_scrolling_up(
+                                    ...
+                                )
+                            end,
+                            ["<C-j>"] = function(...)
+                                return require("telescope.actions").move_selection_next(
+                                    ...
+                                )
+                            end,
+                            ["<C-k>"] = function(...)
+                                return require("telescope.actions").move_selection_previous(
+                                    ...
+                                )
+                            end,
+                        },
+                    },
+                },
+            })
+        end,
+        keys = {
+            -- general
+            {
+                "<leader>ff",
+                "<cmd>Telescope find_files<cr>",
+                desc = "find files",
+            },
+            {
+                "<leader>fr",
+                "<cmd>Telescope oldfiles<cr>",
+                desc = "find files",
+            },
+            {
+                "<leader>fp",
+                function()
+                    require("telescope.builtin").builtin()
+                end,
+                desc = "find pickers",
+            },
+            {
+                "<leader>fb",
+                "<cmd>Telescope buffers<cr>",
+                desc = "find Buffers",
+            },
+            {
+                "<leader>fh",
+                "<cmd>Telescope help_tags<cr>",
+                desc = "help tags",
+            },
+            {
+                "<leader>fg",
+                "<cmd>Telescope live_grep<cr>",
+                desc = "live grep",
+            },
+            {
+                "<leader>fG",
+                function()
+                    require("telescope.builtin").live_grep({
+                        search_dirs = { vim.fn.expand("%:p") },
+                    })
+                end,
+                desc = "live grep current file",
+            },
+            {
+                "<leader>fk",
+                "<cmd>Telescope keymaps<cr>",
+                desc = "find keymaps",
+            },
+            { "<leader>fm", "<cmd>Telescope marks<cr>", desc = "find marks" },
+            {
+                "<leader>fM",
+                "<cmd>Telescope man_pages<cr>",
+                desc = "find manpages",
+            },
+            {
+                "<leader>fs",
+                function()
+                    require("telescope.builtin").lsp_document_symbols({
+                        symbols = {
+                            "Class",
+                            "Function",
+                            "Method",
+                            "Constructor",
+                            "Interface",
+                            "Module",
+                            "Struct",
+                            "Trait",
+                            "Field",
+                            "Property",
+                        },
+                    })
+                end,
+                desc = "find symbols",
+            },
 
-			-- git
-			{ "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "find files" },
-			{ "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "status" },
-			{ "<leader>gS", "<cmd>Telescope git_stash<cr>", desc = "stash" },
-			{ "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "commits" },
-			{ "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "branches" },
-		},
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = "<cmd>TSUpdate<cr>",
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-			{
-				"nvim-treesitter/nvim-treesitter-textobjects",
-				init = function()
-					require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-					load_textobjects = true
-				end,
-			},
-			"nvim-treesitter/nvim-treesitter-context",
-		},
-		opts = {
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"c",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"luadoc",
-				"luap",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-		},
-		config = function(_, opts)
-			if type(opts.ensure_installed) == "table" then
-				local added = {}
-				opts.ensure_installed = vim.tbl_filter(function(lang)
-					if added[lang] then
-						return false
-					end
-					added[lang] = true
-					return true
-				end, opts.ensure_installed)
-			end
-			require("nvim-treesitter.configs").setup(opts)
+            -- git
+            {
+                "<leader>gf",
+                "<cmd>Telescope git_files<cr>",
+                desc = "find files",
+            },
+            { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "status" },
+            { "<leader>gS", "<cmd>Telescope git_stash<cr>", desc = "stash" },
+            {
+                "<leader>gc",
+                "<cmd>Telescope git_commits<cr>",
+                desc = "commits",
+            },
+            {
+                "<leader>gb",
+                "<cmd>Telescope git_branches<cr>",
+                desc = "branches",
+            },
+        },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = "<cmd>TSUpdate<cr>",
+        dependencies = {
+            "JoosepAlviste/nvim-ts-context-commentstring",
+            {
+                "nvim-treesitter/nvim-treesitter-textobjects",
+                init = function()
+                    require("lazy.core.loader").disable_rtp_plugin(
+                        "nvim-treesitter-textobjects"
+                    )
+                    load_textobjects = true
+                end,
+            },
+            "nvim-treesitter/nvim-treesitter-context",
+        },
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+        },
+        config = function(_, opts)
+            if type(opts.ensure_installed) == "table" then
+                local added = {}
+                opts.ensure_installed = vim.tbl_filter(function(lang)
+                    if added[lang] then
+                        return false
+                    end
+                    added[lang] = true
+                    return true
+                end, opts.ensure_installed)
+            end
+            require("nvim-treesitter.configs").setup(opts)
 
-			if load_textobjects then
-				if opts.textobjects then
-					for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-						if opts.textobjects[mod] and opts.textobjects[mod].enable then
-							local Loader = require("lazy.core.loader")
-							Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
-							local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
-							require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
-							break
-						end
-					end
-				end
-			end
-		end,
-	},
+            if load_textobjects then
+                if opts.textobjects then
+                    for _, mod in ipairs({
+                        "move",
+                        "select",
+                        "swap",
+                        "lsp_interop",
+                    }) do
+                        if
+                            opts.textobjects[mod]
+                            and opts.textobjects[mod].enable
+                        then
+                            local Loader = require("lazy.core.loader")
+                            Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] =
+                                nil
+                            local plugin =
+                                require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
+                            require("lazy.core.loader").source_runtime(
+                                plugin.dir,
+                                "plugin"
+                            )
+                            break
+                        end
+                    end
+                end
+            end
+        end,
+    },
 }
--- lua/yoolayn/plugins/ui.lua
+++ lua/yoolayn/plugins/ui.lua
@@ -1,119 +1,127 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		event = "VeryLazy",
-		opts = {
-			options = {
-				globalstatus = true,
-			},
-		},
-	},
-	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = { "BufReadPost", "BufNewFile" },
-		opts = {
-			filetype_exclude = {
-				"help",
-				"alpha",
-				"dashboard",
-				"neo-tree",
-				"Trouble",
-				"lazy",
-				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
-			},
-			show_end_of_line = true,
-			space_char_blankline = " ",
-		},
-		config = function(default, opts)
-			vim.opt.list = true
-			vim.opt.listchars:append("eol:↴")
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        event = "VeryLazy",
+        opts = {
+            options = {
+                globalstatus = true,
+            },
+            sections = {
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+            },
+        },
+    },
+    {
+        "echasnovski/mini.indentscope",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            -- symbol = "▏",
+            symbol = "│",
+            options = { try_as_border = true },
+        },
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                },
+                callback = function()
+                    vim.b.miniindentscope_disable = true
+                end,
+            })
+        end,
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            filetype_exclude = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
+            },
+            show_end_of_line = true,
+            space_char_blankline = " ",
+        },
+        config = function(default, opts)
+            vim.opt.list = true
+            vim.opt.listchars:append("eol:↴")
 
-			local options = vim.tbl_deep_extend("force", default, opts)
-			require("indent_blankline").setup(options)
-		end,
-	},
-	{
-		"stevearc/dressing.nvim",
-	},
-	{
-		"folke/noice.nvim",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-		},
-		event = "VeryLazy",
-		config = function()
-			require("noice").setup({
-				lsp = {
-					override = {
-						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-						["vim.lsp.util.stylize_markdown"] = true,
-						["cmp.entry.get_documentation"] = true,
-					},
-				},
-				routes = {
-					{
-						filter = {
-							event = "msg_show",
-							any = {
-								{ find = "%d+L, %d+B" },
-								{ find = "; after #%d+" },
-								{ find = "; before #%d+" },
-							},
-						},
-						view = "mini",
-					},
-				},
-				presets = {
-					bottom_search = true,
-					long_message_to_split = true,
-					inc_rename = true,
-				},
-				cmdline = {
-					view = "cmdline",
-					format = {
-						search_down = {
-							view = "cmdline",
-						},
-						search_up = {
-							view = "cmdline",
-						},
-					},
-				},
-			})
-		end,
+            local options = vim.tbl_deep_extend("force", default, opts)
+            require("indent_blankline").setup(options)
+        end,
+    },
+    {
+        "stevearc/dressing.nvim",
+    },
+    {
+        "folke/noice.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        event = "VeryLazy",
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
+                },
+                routes = {
+                    {
+                        filter = {
+                            event = "msg_show",
+                            any = {
+                                { find = "%d+L, %d+B" },
+                                { find = "; after #%d+" },
+                                { find = "; before #%d+" },
+                            },
+                        },
+                        view = "mini",
+                    },
+                },
+                presets = {
+                    bottom_search = true,
+                    long_message_to_split = true,
+                    inc_rename = true,
+                },
+                cmdline = {
+                    view = "cmdline",
+                    format = {
+                        search_down = {
+                            view = "cmdline",
+                        },
+                        search_up = {
+                            view = "cmdline",
+                        },
+                    },
+                },
+            })
+        end,
 		-- stylua: ignore
 		keys = {
 			{ "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Redirect Cmdline" },
@@ -124,24 +132,24 @@ return {
 			{ "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
 			{ "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
 		},
-	},
-	{
-		"anuvyklack/help-vsplit.nvim",
-		opts = {},
-	},
-	{
-		"JellyApple102/easyread.nvim",
-		opts = {
-			filetypes = {},
-		},
-		cmd = "EasyreadToggle",
-		keys = {
-			{
-				"<leader>ur",
-				":EasyreadToggle<CR>",
-				desc = "Bionic Reading",
-			},
-		},
-		ft = "txt",
-	},
+    },
+    {
+        "anuvyklack/help-vsplit.nvim",
+        opts = {},
+    },
+    {
+        "JellyApple102/easyread.nvim",
+        opts = {
+            filetypes = {},
+        },
+        cmd = "EasyreadToggle",
+        keys = {
+            {
+                "<leader>ur",
+                ":EasyreadToggle<CR>",
+                desc = "Bionic Reading",
+            },
+        },
+        ft = "txt",
+    },
 }
