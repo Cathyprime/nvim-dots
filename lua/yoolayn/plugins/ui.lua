829eaf06ec08da904e147c9c818bc79ca7864b92
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-20 19:28:51 +0200

    feat: visual indent guide

--- lua/yoolayn/plugins/ui.lua
+++ lua/yoolayn/plugins/ui.lua
@@ -39,6 +39,28 @@ return {
 		end,
 	},
 	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			-- char = "▏",
+			char = "│",
+			filetype_exclude = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+			show_trailing_blankline_indent = false,
+			show_current_context = false,
+		},
+	},
+	{
 		"stevearc/dressing.nvim",
 	},
 	{
