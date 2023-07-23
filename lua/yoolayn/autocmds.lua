028a7745b1e069145b19debf850aa53a4120cd08
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-23 17:17:00 +0200

    style(autocmds.lua): formatting

--- lua/yoolayn/autocmds.lua
+++ lua/yoolayn/autocmds.lua
@@ -1,44 +1,63 @@
 local function augroup(name)
-	return vim.api.nvim_create_augroup("yoolayn_" .. name, { clear = true })
+    return vim.api.nvim_create_augroup("yoolayn_" .. name, { clear = true })
 end
 
+vim.api.nvim_create_autocmd("RecordingEnter", {
+    group = augroup("MacroMessageOn"),
+    callback = function()
+        print("Recording started!")
+    end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+    group = augroup("MacroMessageOff"),
+    callback = function()
+        print("Recording ended!")
+    end,
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = augroup("highlight_yank"),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    group = augroup("highlight_yank"),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	group = augroup("close_with_q"),
-	pattern = {
-		"PlenaryTestPopup",
-		"help",
-		"lspinfo",
-		"man",
-		"notify",
-		"qf",
-		"spectre_panel",
-		"startuptime",
-		"tsplayground",
-		"neotest-output",
-		"checkhealth",
-		"neotest-summary",
-		"neotest-output-panel",
-	},
-	callback = function(event)
-		vim.bo[event.buf].buflisted = false
-		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-	end,
+    group = augroup("close_with_q"),
+    pattern = {
+        "PlenaryTestPopup",
+        "help",
+        "lspinfo",
+        "man",
+        "notify",
+        "qf",
+        "spectre_panel",
+        "startuptime",
+        "tsplayground",
+        "neotest-output",
+        "checkhealth",
+        "neotest-summary",
+        "neotest-output-panel",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set(
+            "n",
+            "q",
+            "<cmd>close<cr>",
+            { buffer = event.buf, silent = true }
+        )
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	group = augroup("auto_create_dir"),
-	callback = function(event)
-		if event.match:match("^%w%w+://") then
-			return
-		end
-		local file = vim.loop.fs_realpath(event.match) or event.match
-		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-	end,
+    group = augroup("auto_create_dir"),
+    callback = function(event)
+        if event.match:match("^%w%w+://") then
+            return
+        end
+        local file = vim.loop.fs_realpath(event.match) or event.match
+        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    end,
 })
