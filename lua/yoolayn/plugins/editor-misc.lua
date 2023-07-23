da2e34e80685ec70dadd488f3e5edf61318cb1eb
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-24 01:39:24 +0200

    feat(mini.surround): better keybinds

--- lua/yoolayn/plugins/editor-misc.lua
+++ lua/yoolayn/plugins/editor-misc.lua
@@ -64,32 +64,15 @@ return {
     },
     {
         "echasnovski/mini.surround",
-        keys = function(_, keys)
-            -- Populate the keys based on the user's options
-            local plugin =
-                require("lazy.core.config").spec.plugins["mini.surround"]
-            local opts =
-                require("lazy.core.plugin").values(plugin, "opts", false)
-            local mappings = {
-                {
-                    opts.mappings.add,
-                    desc = "Add surrounding",
-                    mode = { "n", "v" },
-                },
-                { opts.mappings.delete, desc = "Delete surrounding" },
-                { opts.mappings.replace, desc = "Replace surrounding" },
-            }
-            mappings = vim.tbl_filter(function(m)
-                return m[1] and #m[1] > 0
-            end, mappings)
-            return vim.list_extend(mappings, keys)
-        end,
         opts = {
             mappings = {
-                add = "gza", -- Add surrounding in Normal and Visual modes
-                delete = "gzd", -- Delete surrounding
-                replace = "gzr", -- Replace surrounding
+                find = nil,
+                find_left = nil,
+                highlight = nil,
+                update_n_line = nil,
             },
+            n_lines = nil,
+            respect_selection_type = true,
         },
     },
     {
