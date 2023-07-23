3a23f28214a043c50c2248b420f64998e993bbe8
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-23 18:35:00 +0200

    feat(statusline): mini.statusline

--- lua/yoolayn/plugins/ui.lua
+++ lua/yoolayn/plugins/ui.lua
@@ -1,5 +1,11 @@
 return {
     {
+        "echasnovski/mini.statusline",
+        opts = {
+            set_vim_settings = false,
+        },
+    },
+    {
         "echasnovski/mini.indentscope",
         version = false, -- wait till new 0.7.0 release to put it back on semver
         event = { "BufReadPre", "BufNewFile" },
