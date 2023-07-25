c03877a679daee86a380ef96cdfc528d8f28e9e8
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-26 00:16:15 +0200

    feat(colorschemes): borderless telescope for kanagawa and tokyonight transparency for them too

--- lua/yoolayn/plugins/colorscheme.lua
+++ lua/yoolayn/plugins/colorscheme.lua
@@ -20,6 +20,33 @@ return {
     },
     {
         "rebelot/kanagawa.nvim",
+        opts = {
+            transparent = true,
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = {
+                        fg = theme.ui.bg_p1,
+                        bg = theme.ui.bg_p1,
+                    },
+                    TelescopeResultsNormal = {
+                        fg = theme.ui.fg_dim,
+                        bg = theme.ui.bg_m1,
+                    },
+                    TelescopeResultsBorder = {
+                        fg = theme.ui.bg_m1,
+                        bg = theme.ui.bg_m1,
+                    },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = {
+                        bg = theme.ui.bg_dim,
+                        fg = theme.ui.bg_dim,
+                    },
+                }
+            end,
+        },
         lazy = true,
     },
     {
@@ -36,6 +63,39 @@ return {
     },
     {
         "folke/tokyonight.nvim",
+        opts = {
+            transparent = true,
+            on_highlights = function(hl, c)
+                local prompt = "#2d3149"
+                hl.TelescopeNormal = {
+                    bg = c.bg_dark,
+                    fg = c.fg_dark,
+                }
+                hl.TelescopeBorder = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+                hl.TelescopePromptNormal = {
+                    bg = prompt,
+                }
+                hl.TelescopePromptBorder = {
+                    bg = prompt,
+                    fg = prompt,
+                }
+                hl.TelescopePromptTitle = {
+                    bg = prompt,
+                    fg = prompt,
+                }
+                hl.TelescopePreviewTitle = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+                hl.TelescopeResultsTitle = {
+                    bg = c.bg_dark,
+                    fg = c.bg_dark,
+                }
+            end,
+        },
         lazy = true,
     },
     {
