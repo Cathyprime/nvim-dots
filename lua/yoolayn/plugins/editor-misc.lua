c62de4a63a15a27047478d319b62c9179a92c6bb
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-28 22:54:38 +0200

    fix(trouble): missing dependancy

--- lua/yoolayn/plugins/editor-misc.lua
+++ lua/yoolayn/plugins/editor-misc.lua
@@ -191,6 +191,7 @@ return {
     },
     {
         "folke/trouble.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
         cmd = { "TroubleToggle", "Trouble" },
         opts = { use_diagnostic_signs = true },
         keys = {
