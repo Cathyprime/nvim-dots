8c48bae07c3fce6e28200ab24ec6112b848b7fd7
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-29 10:57:04 +0200

    feat(highligh): custom highlight setup

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require("yoolayn.globals")
 require("yoolayn.options")
 require("yoolayn.keymaps")
+require("yoolayn.highlights")
 require("yoolayn.lazy")
 require("lsp-setup")
 require("yoolayn.colors")
