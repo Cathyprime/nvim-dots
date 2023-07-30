3a6360400733d6de2665534d3b5976a9f5f46c90
Yoolayn <jaroszynskijulian@gmail.com>
2023-07-30 17:17:04 +0200

    BREAKING CHANGE: remove neogit

--- lua/yoolayn/plugins/git.lua
+++ lua/yoolayn/plugins/git.lua
@@ -18,18 +18,4 @@ return {
     {
         "tpope/vim-fugitive",
     },
-    {
-        "NeogitOrg/neogit",
-        dependencies = "nvim-lua/plenary.nvim",
-        opts = {
-            disable_signs = true,
-        },
-        keys = {
-            {
-                "<leader>gg",
-                "<cmd>Neogit<cr>",
-                desc = "Open Neogit",
-            },
-        },
-    },
 }
