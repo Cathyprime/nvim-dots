500b9ddc4b3f880083ef62b88356adbd5593daa2
Cathyprime <magdajaroszynska47@gmail.com>
2023-07-30 17:30:06 +0200

    BREAKING CHANGE: disable flash for testing if it's really needed

--- lua/yoolayn/plugins/editor-misc.lua
+++ lua/yoolayn/plugins/editor-misc.lua
@@ -177,18 +177,18 @@ return {
             end)
         end,
     },
-    {
-        "folke/flash.nvim",
-        opts = {},
-		-- stylua: ignore
-		keys = {
-			{ "gs", mode = { "n", "o", "x"}, function() require("flash").jump() end, desc = "Flash" },
-			{ "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-		},
-    },
+    --   {
+    --       "folke/flash.nvim",
+    --       opts = {},
+    -- -- stylua: ignore
+    -- keys = {
+    -- 	{ "gs", mode = { "n", "o", "x"}, function() require("flash").jump() end, desc = "Flash" },
+    -- 	{ "S", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    -- 	{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    -- 	{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    -- 	{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- },
+    --   },
     {
         "folke/trouble.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
