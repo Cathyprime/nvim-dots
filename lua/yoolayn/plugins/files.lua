7f79fdaf7b002dfb928b2365eb33e78ec67b867d
Yoolayn <115636257+Yoolayn@users.noreply.github.com>
2023-07-30 20:26:30 +0200

    fix(files): rebind oil (#14)

--- lua/yoolayn/plugins/files.lua
+++ lua/yoolayn/plugins/files.lua
@@ -3,11 +3,11 @@ return {
         "stevearc/oil.nvim",
         keys = {
             {
-                "<leader>e",
+                "<leader>ef",
                 function()
                     require("oil").open()
                 end,
-                desc = "open files",
+                desc = "explore files",
             },
         },
         opts = {},
@@ -16,9 +16,9 @@ return {
         "nvim-pack/nvim-spectre",
         cmd = "Spectre",
         opts = { open_cmd = "noswapfile vnew" },
-		-- stylua: ignore
-		keys = {
-			{ "<leader>ur", function() require("spectre").open() end, desc = "Replace in files (spectre)" },
-		},
+        -- stylua: ignore
+        keys = {
+            { "<leader>ur", function() require("spectre").open() end, desc = "Replace in files (spectre)" },
+        },
     },
 }
