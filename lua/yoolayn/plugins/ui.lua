13ffd385f0e662736a7a15b709a435b0f423e7ab
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-08 00:21:20 +0200

    fix(plugins): remove indent plugins

--- lua/yoolayn/plugins/ui.lua
+++ lua/yoolayn/plugins/ui.lua
@@ -39,56 +39,6 @@ return {
         },
     },
     {
-        "echasnovski/mini.indentscope",
-        version = false,
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            -- symbol = "▏",
-            symbol = "│",
-            options = { try_as_border = true },
-        },
-        init = function()
-            vim.api.nvim_create_autocmd("FileType", {
-                pattern = {
-                    "help",
-                    "Trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-                callback = function()
-                    vim.b.miniindentscope_disable = true
-                end,
-            })
-        end,
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            filetype_exclude = {
-                "help",
-                "Trouble",
-                "lazy",
-                "mason",
-                "notify",
-                "toggleterm",
-                "lazyterm",
-            },
-            show_end_of_line = true,
-            space_char_blankline = " ",
-        },
-        config = function(default, opts)
-            vim.opt.list = true
-            vim.opt.listchars:append("eol:↴")
-
-            local options = vim.tbl_deep_extend("force", default, opts)
-            require("indent_blankline").setup(options)
-        end,
-    },
-    {
         "stevearc/dressing.nvim",
         event = "VeryLazy",
     },
