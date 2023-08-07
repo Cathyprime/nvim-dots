0931651ed3876acd9d96159d1d4b986966c1b6d4
Cathyprime <magdajaroszynska47@gmail.com>
2023-08-07 23:29:47 +0200

    BREAKING CHANGE: remove spectre

--- lua/yoolayn/plugins/files.lua
+++ lua/yoolayn/plugins/files.lua
@@ -12,13 +12,4 @@ return {
         },
         opts = {},
     },
-    {
-        "nvim-pack/nvim-spectre",
-        cmd = "Spectre",
-        opts = { open_cmd = "noswapfile vnew" },
-        -- stylua: ignore
-        keys = {
-            { "<leader>ur", function() require("spectre").open() end, desc = "Replace in files (spectre)" },
-        },
-    },
 }
