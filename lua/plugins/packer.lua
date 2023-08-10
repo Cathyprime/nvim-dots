9d1f739d5d9e16e6268d32e6564bab59b06d732c
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-11 01:04:15 +0200

    feat: git integration

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -74,4 +74,21 @@ return require('packer').startup(function(use)
     -- file navigations
     use({"ThePrimeagen/harpoon"})
     use({"stevearc/oil.nvim"})
+
+    -- git
+    use({ "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local function map(mode, lhs, rhs, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, lhs, rhs, opts)
+                    end
+                    -- stylua: ignore
+                    map({"o", "x"}, "ih", ":<C-u>Gitsigns select_hunk", {desc = "hunk"})
+                end,
+            })
+        end})
+    use({"tpope/vim-fugitive"})
 end)
