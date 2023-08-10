1a2219aea72921c59a2267eda4611bca9e7e3066
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-10 23:13:48 +0200

    feat(plugins): packer setup + telescope

---
+++ lua/plugins/after/telescope.lua
@@ -0,0 +1,95 @@
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {desc = "find files" })
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", {desc = "find recent files" })
+vim.keymap.set("n", "<leader>fp", function() require("telescope.builtin").builtin() end, {desc = "find pickers" })
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {desc = "find Buffers" })
+vim.keymap.set("n", "<leader>fh", function ()
+    local vhelp = vim.api.nvim_create_augroup("Vertical Help", {})
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = vhelp,
+        callback = function ()
+            if vim.bo.filetype == "help" then
+                vim.cmd("wincmd L")
+                vim.api.nvim_clear_autocmds({group = vhelp})
+            end
+        end
+    })
+    require("telescope.builtin").help_tags()
+end, {desc = "vertical help tags" })
+
+vim.keymap.set("n", "<leader>fH", function ()
+    if next(vim.api.nvim_get_autocmds({group = "Vertical Help"})) ~= nil then
+        vim.api.nvim_clear_autocmds({ group = "Vertical Help" })
+    end
+    require("telescope.builtin").help_tags()
+end, {desc = "help tags" })
+
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", {desc = "live grep" })
+vim.keymap.set("n", "<leader>fG", function()
+        require("telescope.builtin").live_grep({
+            search_dirs = { vim.fn.expand("%:p") },
+        })
+    end,
+    {desc = "live grep current file" })
+
+vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", {desc = "find keymaps" })
+vim.keymap.set("n", "<leader>fm", "<cmd>Telescope marks<cr>", {desc = "find marks" })
+vim.keymap.set("n", "<leader>fs", function()
+        require("telescope.builtin").lsp_document_symbols({
+            symbols = {
+                "Class",
+                "Function",
+                "Method",
+                "Constructor",
+                "Interface",
+                "Module",
+                "Struct",
+                "Trait",
+                "Field",
+                "Property",
+            },
+        })
+    end,
+    {desc = "find symbols"})
+
+-- git
+vim.keymap.set("n", "<c-p>", "<cmd>Telescope git_files<cr>", {desc = "find git files" })
+vim.keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<cr>", {desc = "find git files" })
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", {desc = "git status" })
+vim.keymap.set("n", "<leader>gS", "<cmd>Telescope git_stash<cr>", {desc = "git stash" })
+vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", {desc = "git commits" })
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", {desc = "git branches" })
+
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-d>"] = function(...)
+                    return require("telescope.actions").preview_scrolling_down(
+                    ...
+                    )
+                end,
+                ["<C-u>"] = function(...)
+                    return require("telescope.actions").preview_scrolling_up(
+                    ...
+                    )
+                end,
+                ["<C-j>"] = function(...)
+                    return require("telescope.actions").move_selection_next(
+                    ...
+                    )
+                end,
+                ["<C-k>"] = function(...)
+                    return require("telescope.actions").move_selection_previous(
+                    ...
+                    )
+                end,
+                --<esc>["<C-t>"] = require("trouble.providers.telescope").open_with_trouble,
+            },
+            n = {
+                --<esc>["<C-t>"] = require("trouble.providers.telescope").open_with_trouble,
+            },
+        },
+    },
+})
