0b910f7100fc24246b94212bff5b5de3b611087e
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-11 02:09:20 +0200

    feat: lsp additions

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -106,7 +106,11 @@ return require('packer').startup(function(use)
             {"hrsh7th/nvim-cmp"},     -- Required
             {"hrsh7th/cmp-nvim-lsp"}, -- Required
             {"L3MON4D3/LuaSnip"},     -- Required
-        }
+            {"rafamadriz/friendly-snippets",
+            config = function () require("luasnip.loaders.from_vscode").lazy_load() end},
+            {"hrsh7th/cmp-path"},
+            {"dgagn/diagflow.nvim",
+            config = function () require("diagflow").setup({ scope = "line", }) end }}
     })
 
     use({"folke/neodev.nvim",
