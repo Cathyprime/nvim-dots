958a98db8396f0c60f33b391ef752688e8867aab
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-13 15:05:45 +0200

    fix(packer): self bootstrap

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,4 +1,15 @@
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     use({'wbthomason/packer.nvim'})
@@ -172,4 +183,7 @@ return require('packer').startup(function(use)
             vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end)
             vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end)
         end})
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
