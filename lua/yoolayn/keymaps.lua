ba1c4504cab1ba4a320a06d976997354e1a190f7
Cathyprime <magdajaroszynska47@gmail.com>
2023-08-14 13:50:12 +0200

    feat(keymaps): add comments

--- lua/yoolayn/keymaps.lua
+++ lua/yoolayn/keymaps.lua
@@ -7,25 +7,29 @@ vim.keymap.set("n", "<leader>ef", vim.cmd.Ex, {desc = "explore files"})
 vim.keymap.set("n", "<C-b>", "<Nop>")
 vim.keymap.set("n", "<C-f>", "<Nop>")
 
+-- keep cursor position on joining lines
 vim.keymap.set("n", "J", "mzJ`z", { desc = "join lines" })
+
+-- recenter screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "scroll down" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "scroll up" })
 vim.keymap.set("n", "n", "nzzzv", { desc = "next search" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "prev search" })
 
--- stylua: ignore start
+-- line text objects
 vim.keymap.set({ "o", "x" }, "il", ":<c-u>normal! _vg_<cr>", { desc = "inner line", silent = true })
 vim.keymap.set({ "o", "x" }, "al", ":<c-u>normal! 0v$<cr>", { desc = "around line", silent = true })
 
--- stylua: ignore
+-- clipboard interaction
 vim.keymap.set( { "n", "v" }, "<leader>y", [["+y]], { desc = "yank to clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
 vim.keymap.set("n", "<leader>p", [["+p]], { desc = "paste from clipboard" })
 vim.keymap.set("n", "<leader>P", [["+P]], { desc = "Paste from clipboard" })
 
--- style: ignore
+-- blackhole register
 vim.keymap.set( { "n", "v" }, "<leader>d", [["_d]], { desc = "delete to black hole" })
 
+-- substitute commands
 vim.keymap.set(
     "n",
     "<leader>s",
