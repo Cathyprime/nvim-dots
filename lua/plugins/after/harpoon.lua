b7b839041c068fd4bb7c34bd96e72850f874b4da
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-17 23:00:05 +0200

    feat: migrate back to lazy.nvim

--- lua/plugins/after/harpoon.lua
+++ lua/plugins/after/harpoon.lua
@@ -8,22 +8,71 @@ local function send(command)
     tmux.gotoTerminal(term)
 end
 
--- harpoon usage
-vim.keymap.set("n" , "<leader>h", function() ui.toggle_quick_menu() end, {desc = "harpoon", })
-vim.keymap.set("n" , "<leader>a", function() mark.add_file() end, {desc = "add file", })
 
--- file navigation
-vim.keymap.set("n" , "<C-f>", function() ui.nav_file(1) end, {desc = "go to first", })
-vim.keymap.set("n" , "<C-s>", function() ui.nav_file(2) end, {desc = "go to second", })
-vim.keymap.set("n" , "<c-n>", function() ui.nav_file(3) end, {desc = "go to next", })
-vim.keymap.set("n" , "<c-h>", function() ui.nav_file(4) end, {desc = "go to harpoon", })
+return {
+    "ThePrimeagen/harpoon",
+    keys = {
+        -- harpoon usage
+        {
+            "<leader>h",
+            function() ui.toggle_quick_menu() end,
+            {desc = "harpoon", }
+        },
+        {
+            "<leader>a",
+            function() mark.add_file() end,
+            {desc = "add file", }
+        },
 
--- tmux integration
-vim.keymap.set("n", "<c-j>", function () tmux.gotoTerminal(term) end, {desc = "go to term"})
-vim.keymap.set("n", "<c-k>", function () tmux.gotoTerminal(2) end, {desc = "go to second term"})
-vim.keymap.set("n" ,"<leader>tc", function()
-    local command = vim.fn.input({ prompt = "bash: " })
-    send(command)
-end, {desc = "tmux custom", })
-vim.keymap.set("n", "<leader>ty", function () send("yarn") end, {desc = "tmux yarn"})
-vim.keymap.set("n", "<leader>tr", function () send("cargo") end, {desc = "tmux rust"})
+        -- file navigation
+        {
+            "<C-f>",
+            function() ui.nav_file(1) end,
+            {desc = "go to first", }
+        },
+        {
+            "<C-s>",
+            function() ui.nav_file(2) end,
+            {desc = "go to second", }
+        },
+        {
+            "<c-n>",
+            function() ui.nav_file(3) end,
+            { desc = "go to next" }
+        },
+        {
+            "<c-h>",
+            function() ui.nav_file(4) end,
+            {desc = "go to harpoon", }
+        },
+
+        -- tmux integration
+        {
+            "<c-j>",
+            function () tmux.gotoTerminal(term) end,
+            {desc = "go to term"}
+        },
+        {
+            "<c-k>",
+            function () tmux.gotoTerminal(2) end,
+            {desc = "go to second term"}
+        },
+        {
+            "<leader>tc",
+            function()
+                local command = vim.fn.input({ prompt = "bash: " })
+                send(command)
+            end,
+            {desc = "tmux custom", }},
+        {
+            "<leader>ty",
+            function () send("yarn") end,
+            { desc = "tmux yarn" }
+        },
+        {
+            "<leader>tr",
+            function () send("cargo") end,
+            {desc = "tmux rust"}
+        },
+    }
+}
--- lua/plugins/after/treesitter-textobj.lua
+++ lua/plugins/after/treesitter-textobj.lua
@@ -1,41 +1,47 @@
----@diagnostic disable-next-line
-require("nvim-treesitter.configs").setup {
-    textobjects = {
-        select = {
-            enable = true,
-            lookahead = true,
+return {
+    "nvim-treesitter/nvim-treesitter-textobjects",
 
-            keymaps = {
-                ["af"] = { query = "@function.outer", desc = "select a functions" },
-                ["if"] = { query = "@function.inner", desc = "select inner functions" },
-                ["ac"] = { query = "@class.outer", desc = "select a class" },
-                ["ic"] = { query = "@class.inner", desc = "select inner class" },
-                ["as"] = { query = "@scope", query_group = "locals", desc = "select language scope" },
-                ["aa"] = { query = "@parameter.outer", desc = "select a argument" },
-                ["ia"] = { query = "@parameter.inner", desc = "select inner argument" },
+    config = function ()
+        ---@diagnostic disable-next-line
+        require("nvim-treesitter.configs").setup {
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+
+                    keymaps = {
+                        ["af"] = { query = "@function.outer", desc = "select a functions" },
+                        ["if"] = { query = "@function.inner", desc = "select inner functions" },
+                        ["ac"] = { query = "@class.outer", desc = "select a class" },
+                        ["ic"] = { query = "@class.inner", desc = "select inner class" },
+                        ["as"] = { query = "@scope", query_group = "locals", desc = "select language scope" },
+                        ["aa"] = { query = "@parameter.outer", desc = "select a argument" },
+                        ["ia"] = { query = "@parameter.inner", desc = "select inner argument" },
+                    },
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true of false
+                    include_surrounding_whitespace = true,
+                },
             },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V', -- linewise
-                ['@class.outer'] = '<c-v>', -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true of false
-            include_surrounding_whitespace = true,
-        },
-    },
+        }
+    end
 }
