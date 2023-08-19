ae0fb603b775417309f0d56a0a05b2a5b7a0e350
Cathyprime <magdajaroszynska47@gmail.com>
2023-08-19 17:50:27 +0200

    feat(scala): install

---
+++ lua/plugins/after/scala.lua
@@ -0,0 +1,21 @@
+local nvim_metals_group =
+    vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "scala", "sbt", "java" },
+    callback = function()
+        require("metals").initialize_or_attach({})
+        local metals_config = require("metals").bare_config()
+        metals_config.settings = {
+            showImplicitArguments = true,
+            serverVersion = "latest.snapshot",
+        }
+        metals_config.init_options.statusBarProvider = "on"
+    end,
+    group = nvim_metals_group,
+})
+
+return {
+    "scalameta/nvim-metals",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    ft = "scala",
+}
