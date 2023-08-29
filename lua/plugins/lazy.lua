9f3d7ce9a2c0689af5f9bd269ec5610b14ffc46f
Yoolayn <jaroszynskijulian@gmail.com>
2023-08-29 14:39:54 +0200

    fix(lazy): formatting

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -66,7 +66,11 @@ require("lazy").setup({
         },
 
         -- helpers for editing
-        { "numToStr/Comment.nvim", config = function() require('Comment').setup() end, keys = { "gc", "gcA", {"gc", mode="v"} } },
+        {
+            "numToStr/Comment.nvim",
+            config = function() require('Comment').setup() end,
+            keys = { "gc", "gcA", {"gc", mode="v"} },
+        },
         {
             "kylechui/nvim-surround",
             tag = "*",
@@ -77,65 +81,83 @@ require("lazy").setup({
             "echasnovski/mini.pairs",
             config = function() require("mini.pairs").setup() end,
         },
-        {"mbbill/undotree",
-            keys = { { "<leader>u", ":UndotreeToggle<cr>", { silent = true, desc = "toggle undotree" } } } },
-        {"anuvyklack/hydra.nvim"},
+        {
+            "mbbill/undotree",
+            keys = { { "<leader>u", ":UndotreeToggle<cr>", { silent = true, desc = "toggle undotree" } } },
+        },
+        "anuvyklack/hydra.nvim" ,
 
         -- file management
-        {"ThePrimeagen/harpoon"},
-        {"nvim-tree/nvim-tree.lua",
+        "ThePrimeagen/harpoon",
+        {
+            "nvim-tree/nvim-tree.lua",
             dependencies = {"nvim-tree/nvim-web-devicons"},
             opts = { filters = { dotfiles = true } },
-            keys = { { "<leader>e", ":NvimTreeToggle<cr>", {silent = true}} }, },
+            keys = { { "<leader>ef", ":NvimTreeToggle<cr>", { silent = true } } },
+        },
 
         -- git integration
-        {"tpope/vim-fugitive"},
-        {"lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({
-                on_attach = function(bufnr)
-                    vim.keymap.set({"o", "x"}, "ih", ":<C-u>Gitsigns select_hunk<cr>", {desc = "inner hunk", buffer = bufnr})
-                end,
-            })
-        end},
+        "tpope/vim-fugitive",
+        {
+            "lewis6991/gitsigns.nvim",
+            config = function()
+                require("gitsigns").setup({
+                    on_attach = function(bufnr) vim.keymap.set({ "o", "x" }, "ih", ":<C-u>Gitsigns select_hunk<cr>", { desc = "inner hunk", buffer = bufnr }) end,
+                })
+            end
+        },
 
         -- lsp
-        {"VonHeikemen/lsp-zero.nvim",
+        {
+            "VonHeikemen/lsp-zero.nvim",
             branch = "v2.x",
             dependencies = {
                 -- LSP Support
-                {"neovim/nvim-lspconfig"},             -- Required
-                {"williamboman/mason.nvim"},           -- Optional
-                {"williamboman/mason-lspconfig.nvim"}, -- Optional
+                "neovim/nvim-lspconfig",             -- Required
+                "williamboman/mason.nvim",           -- Optional
+                "williamboman/mason-lspconfig.nvim", -- Optional
 
                 -- Autocompletion
-                {"hrsh7th/nvim-cmp"},     -- Required
-                {"hrsh7th/cmp-nvim-lsp"}, -- Required
-                {"L3MON4D3/LuaSnip"},     -- Required
-                {"hrsh7th/cmp-buffer"},
-                {"octaltree/cmp-look"},
-                {"rafamadriz/friendly-snippets",
-                    config = function () require("luasnip.loaders.from_vscode").lazy_load() end},
-                {"hrsh7th/cmp-path"},
-                {"saadparwaiz1/cmp_luasnip"},
-                {"dgagn/diagflow.nvim", config = function () require("diagflow").setup({ scope = "line", }) end }
+                "hrsh7th/nvim-cmp",     -- Required
+                "hrsh7th/cmp-nvim-lsp", -- Required
+                "L3MON4D3/LuaSnip",     -- Required
+                "hrsh7th/cmp-buffer",
+                "octaltree/cmp-look",
+                {
+                    "rafamadriz/friendly-snippets",
+                    config = function () require("luasnip.loaders.from_vscode").lazy_load() end,
+                },
+                "hrsh7th/cmp-path",
+                "saadparwaiz1/cmp_luasnip",
+                {
+                    "dgagn/diagflow.nvim",
+                    config = function () require("diagflow").setup({ scope = "line", }) end,
+                }
             }
         },
-        { "folke/neodev.nvim", config = function () require("neodev").setup() end, ft = "lua" },
-        {'j-hui/fidget.nvim', tag = 'legacy',
-        config = function() require("fidget").setup({
-            text = { spinner = "moon" },
-            window = { blend = 0 } })
-        end},
+        {
+            "folke/neodev.nvim",
+            config = function () require("neodev").setup() end,
+            ft = "lua",
+        },
+        {
+            "j-hui/fidget.nvim", tag = 'legacy',
+            config = function() require("fidget").setup({
+                text = { spinner = "moon" },
+                window = { blend = 0 } })
+            end
+        },
 
         -- ui
-        {"JellyApple102/easyread.nvim",
+        {
+            "JellyApple102/easyread.nvim",
             config = function ()
                 require("easyread").setup({ filetypes = {} })
                 vim.keymap.set("n", "<leader>U", ":EasyreadToggle<cr>", { desc = "toggle easier reading", silent = true })
             end
         },
-        {"folke/todo-comments.nvim",
+        {
+            "folke/todo-comments.nvim",
             dependencies = { "nvim-lua/plenary.nvim" },
             opts = {},
             event = "VimEnter",
@@ -149,8 +171,8 @@ require("lazy").setup({
             ft = "yuck"},
 
         -- misc
-        {"ThePrimeagen/vim-be-good"},
-        {"tpope/vim-dispatch"},
-        {"godlygeek/tabular"},
+        "ThePrimeagen/vim-be-good",
+        "tpope/vim-dispatch",
+        "godlygeek/tabular",
     }
 })
