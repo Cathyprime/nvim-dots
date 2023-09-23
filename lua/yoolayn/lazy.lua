0f33825b276acfa79bcf1d0cd46d87a945772398
Cathyprime <magdajaroszynska47@gmail.com>
2023-09-23 23:48:03 +0200

    feat(luasnip): sh snippets p1

--- lua/yoolayn/lazy.lua
+++ lua/yoolayn/lazy.lua
@@ -128,7 +128,7 @@ require("lazy").setup({
                     "rafamadriz/friendly-snippets",
                     config = function()
                         require("luasnip.loaders.from_vscode").lazy_load({
-                            exclude = { "lua", "rust", "gitcommit" },
+                            exclude = { "lua", "rust", "gitcommit", "sh" },
                         })
                     end
                 },
