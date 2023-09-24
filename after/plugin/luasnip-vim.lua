d206c9bfb4803fafd1ee760dcf12934325cad703
Yoolayn <jaroszynskijulian@gmail.com>
2023-09-24 23:03:34 +0200

    refractor: switch to tabs

--- after/plugin/luasnip-vim.lua
+++ after/plugin/luasnip-vim.lua
@@ -13,120 +13,120 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("vim", {
 
-    s("fn", fmt([[
-    function{clear} {name}()
-        {body}
-    endfunction
-    ]], {
-        clear = c(1, {
-            t"!",
-            t"",
-        }),
-        name = i(2, "name"),
-        body = i(0)
-    })),
+	s("fn", fmt([[
+	function{clear} {name}()
+		{body}
+	endfunction
+	]], {
+		clear = c(1, {
+			t"!",
+			t"",
+		}),
+		name = i(2, "name"),
+		body = i(0)
+	})),
 
-    s("if", fmt([[
-    if {condition}
-        {body}
-    endif
-    ]], {
-        condition = i(1, "conditon"),
-        body = c(2, {
-            i(1),
-            sn(nil, fmt([[
-            {body1}
-            else
-                {body2}
-            ]], {
-                body1 = i(1),
-                body2 = i(2),
-            }))
-        })
-    })),
+	s("if", fmt([[
+	if {condition}
+		{body}
+	endif
+	]], {
+		condition = i(1, "conditon"),
+		body = c(2, {
+			i(1),
+			sn(nil, fmt([[
+			{body1}
+			else
+				{body2}
+			]], {
+				body1 = i(1),
+				body2 = i(2),
+			}))
+		})
+	})),
 
-    s({trig = ".*eif", regTrig = true,}, fmt([[
-    elseif {condition}
-        {body}
-    ]], {
-        condition = i(1, "condition"),
-        body = i(0)
-    })),
+	s({trig = ".*eif", regTrig = true,}, fmt([[
+	elseif {condition}
+		{body}
+	]], {
+		condition = i(1, "condition"),
+		body = i(0)
+	})),
 
-    s("aug", fmt([[
-    augroup {name}
-        {clear}{body}
-    augroup END
-    ]], {
-        clear = c(1, {
-            t{"au!", "\t"},
-            t"",
-        }),
-        name = i(2, "name"),
-        body = i(0)
-    })),
+	s("aug", fmt([[
+	augroup {name}
+		{clear}{body}
+	augroup END
+	]], {
+		clear = c(1, {
+			t{"au!", "\t"},
+			t"",
+		}),
+		name = i(2, "name"),
+		body = i(0)
+	})),
 
-    s("au", fmt([[
-    au {event} {pattern} {command}
-    ]], {
-        event = i(1, "event"),
-        pattern = c(2, {
-            i(1, "pattern"),
-            t"*",
-            t"<buffer>",
-        }),
-        command = i(3, "command")
-    })),
+	s("au", fmt([[
+	au {event} {pattern} {command}
+	]], {
+		event = i(1, "event"),
+		pattern = c(2, {
+			i(1, "pattern"),
+			t"*",
+			t"<buffer>",
+		}),
+		command = i(3, "command")
+	})),
 
-    s("while", fmt([[
-    while {condition}
-        {body}
-    endwhile
-    ]], {
-        condition = i(1, "condition"),
-        body = i(0)
-    })),
+	s("while", fmt([[
+	while {condition}
+		{body}
+	endwhile
+	]], {
+		condition = i(1, "condition"),
+		body = i(0)
+	})),
 
-    s("for", fmt([[
-    for {iter} in {iteree}
-        {body}
-    endfor
-    ]], {
-        iter = i(1, "iterator"),
-        iteree = c(2, {
-            sn(nil, fmt([[{}]], { i(1, "variable") })),
-            sn(nil, fmt([[range({}, {})]], {i(1, "start"), i(2, "inclusive")})),
-            sn(nil, fmt("[{}]", i(1, "list"))),
-        }),
-        body = i(0)
-    })),
+	s("for", fmt([[
+	for {iter} in {iteree}
+		{body}
+	endfor
+	]], {
+		iter = i(1, "iterator"),
+		iteree = c(2, {
+			sn(nil, fmt([[{}]], { i(1, "variable") })),
+			sn(nil, fmt([[range({}, {})]], {i(1, "start"), i(2, "inclusive")})),
+			sn(nil, fmt("[{}]", i(1, "list"))),
+		}),
+		body = i(0)
+	})),
 
-    s("if?", fmt([[
-    {condition} ? {expr1} : {expr2}
-    ]], {
-        condition = i(1, "condition"),
-        expr1 = i(2, "expression"),
-        expr2 = i(3, "expression"),
-    })),
+	s("if?", fmt([[
+	{condition} ? {expr1} : {expr2}
+	]], {
+		condition = i(1, "condition"),
+		expr1 = i(2, "expression"),
+		expr2 = i(3, "expression"),
+	})),
 
-    s("map", fmt([[
-    {mode}{nore}map {buffer}{silent}{lhs} {rhs}
-    ]], {
-        mode = i(1, "n"),
-        nore = c(2, {
-            t"nore",
-            t"",
-        }),
-        buffer = c(3, {
-            t"",
-            t"<buffer> ",
-        }),
-        silent = c(4, {
-            t"",
-            t"<silent> ",
-        }),
-        lhs = i(5, "lhs"),
-        rhs = i(6, "rhs"),
-    }))
+	s("map", fmt([[
+	{mode}{nore}map {buffer}{silent}{lhs} {rhs}
+	]], {
+		mode = i(1, "n"),
+		nore = c(2, {
+			t"nore",
+			t"",
+		}),
+		buffer = c(3, {
+			t"",
+			t"<buffer> ",
+		}),
+		silent = c(4, {
+			t"",
+			t"<silent> ",
+		}),
+		lhs = i(5, "lhs"),
+		rhs = i(6, "rhs"),
+	}))
 
 })
--- lua/yoolayn/lazy.lua
+++ lua/yoolayn/lazy.lua
@@ -1,188 +1,188 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        install = {
-            missing = false,
-        },
-        checker = {
-            enabled = false,
-        },
-        change_detection = {
-            notify = false
-        },
-        -- {import = "plugin"}
-        -- telescope stuff
-        {
-            'nvim-telescope/telescope.nvim',
-            tag = '0.1.2',
-            dependencies = { 'nvim-lua/plenary.nvim' },
-        },
+	spec = {
+		install = {
+			missing = false,
+		},
+		checker = {
+			enabled = false,
+		},
+		change_detection = {
+			notify = false
+		},
+		-- {import = "plugin"}
+		-- telescope stuff
+		{
+			'nvim-telescope/telescope.nvim',
+			tag = '0.1.2',
+			dependencies = { 'nvim-lua/plenary.nvim' },
+		},
 
-        -- colorscheme
-        "rebelot/kanagawa.nvim",
+		-- colorscheme
+		"rebelot/kanagawa.nvim",
 
-        -- helpers for editing
-        "cohama/lexima.vim",
-        {
-            "numToStr/Comment.nvim",
-            opts = {},
-        },
-        {
-            "kylechui/nvim-surround",
-            tag = "*",
-            opts = {},
-        },
-        {
-            "mbbill/undotree",
-            config = function ()
-                vim.api.nvim_create_autocmd("VimEnter", {
-                    once = true,
-                    callback = function()
-                        vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>", {silent = true})
-                    end
-                })
-            end
-        },
-        "anuvyklack/hydra.nvim" ,
+		-- helpers for editing
+		"cohama/lexima.vim",
+		{
+			"numToStr/Comment.nvim",
+			opts = {},
+		},
+		{
+			"kylechui/nvim-surround",
+			tag = "*",
+			opts = {},
+		},
+		{
+			"mbbill/undotree",
+			config = function ()
+				vim.api.nvim_create_autocmd("VimEnter", {
+					once = true,
+					callback = function()
+						vim.keymap.set("n", "<leader>u", "<cmd>UndotreeToggle<cr>", {silent = true})
+					end
+				})
+			end
+		},
+		"anuvyklack/hydra.nvim" ,
 
-        -- file management
-        {
-            "nvim-tree/nvim-tree.lua",
-            dependencies = {"nvim-tree/nvim-web-devicons"},
-            config = function()
-                -- NOTE: i have no idea why, but without binding on VimEnter
-                -- it does not bind at all
-                vim.api.nvim_create_autocmd("VimEnter", {
-                    once = true,
-                    callback = function()
-                        vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { silent = true })
-                    end
-                })
-                require("nvim-tree").setup({
-                    filters = {
-                        dotfiles = true,
-                    }
-                })
-            end
-        },
-        "ThePrimeagen/harpoon",
+		-- file management
+		{
+			"nvim-tree/nvim-tree.lua",
+			dependencies = {"nvim-tree/nvim-web-devicons"},
+			config = function()
+				-- NOTE: i have no idea why, but without binding on VimEnter
+				-- it does not bind at all
+				vim.api.nvim_create_autocmd("VimEnter", {
+					once = true,
+					callback = function()
+						vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { silent = true })
+					end
+				})
+				require("nvim-tree").setup({
+					filters = {
+						dotfiles = true,
+					}
+				})
+			end
+		},
+		"ThePrimeagen/harpoon",
 
-        -- git integration
-        "tpope/vim-fugitive",
-        {
-            "lewis6991/gitsigns.nvim",
-            opts = {
-                on_attach = function(bufnr)
-                    vim.keymap.set(
-                    { "o", "x" },
-                    "ih",
-                    ":<C-u>Gitsigns select_hunk<cr>",
-                    {
-                        desc = "inner hunk",
-                        buffer = bufnr
-                    })
-                end,
-            }
-        },
+		-- git integration
+		"tpope/vim-fugitive",
+		{
+			"lewis6991/gitsigns.nvim",
+			opts = {
+				on_attach = function(bufnr)
+					vim.keymap.set(
+					{ "o", "x" },
+					"ih",
+					":<C-u>Gitsigns select_hunk<cr>",
+					{
+						desc = "inner hunk",
+						buffer = bufnr
+					})
+				end,
+			}
+		},
 
-        -- treesitter
-        {
-            "nvim-treesitter/nvim-treesitter",
-            build = ":TSUpdate",
-            dependencies = {
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                "JoosepAlviste/nvim-ts-context-commentstring",
-                "nvim-treesitter/playground",
-            }
-        },
-        -- lsp
-        {
-            "VonHeikemen/lsp-zero.nvim",
-            branch = "v2.x",
-            dependencies = {
-                -- LSP Support
-                "neovim/nvim-lspconfig",             -- Required
-                "williamboman/mason.nvim",           -- Optional
-                "williamboman/mason-lspconfig.nvim", -- Optional
+		-- treesitter
+		{
+			"nvim-treesitter/nvim-treesitter",
+			build = ":TSUpdate",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				"JoosepAlviste/nvim-ts-context-commentstring",
+				"nvim-treesitter/playground",
+			}
+		},
+		-- lsp
+		{
+			"VonHeikemen/lsp-zero.nvim",
+			branch = "v2.x",
+			dependencies = {
+				-- LSP Support
+				"neovim/nvim-lspconfig",			 -- Required
+				"williamboman/mason.nvim",			 -- Optional
+				"williamboman/mason-lspconfig.nvim", -- Optional
 
-                -- Autocompletion
-                "hrsh7th/nvim-cmp",     -- Required
-                "hrsh7th/cmp-nvim-lsp", -- Required
-                "L3MON4D3/LuaSnip",     -- Required
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-nvim-lua",
-                "octaltree/cmp-look",
-                "hrsh7th/cmp-path",
-                {
-                    "dgagn/diagflow.nvim",
-                    opts = {scope = "line"}
-                },
-            },
-        },
-        {
-            "j-hui/fidget.nvim",
-            tag = "legacy",
-            opts = {
-                text = { spinner = "moon" },
-                window = { blend = 0 }
-            }
-        },
-        {
-            "scalameta/nvim-metals",
-            dependencies = { "nvim-lua/plenary.nvim" },
-            config = function()
-                local lsp_zero = require('lsp-zero')
-                local metals_config = require('metals').bare_config()
-                metals_config.capabilities = lsp_zero.get_capabilities()
-                local nvim_metals_group =
-                vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-                vim.api.nvim_create_autocmd("FileType", {
-                    pattern = { "scala", "sbt", "java" },
-                    callback = function()
-                        require("metals").initialize_or_attach({})
-                        local metals_config = require("metals").bare_config()
-                        metals_config.settings = {
-                            showImplicitArguments = true,
-                            serverVersion = "latest.snapshot",
-                        }
-                        metals_config.init_options.statusBarProvider = "on"
-                    end,
-                    group = nvim_metals_group,
-                })
-            end
-        },
+				-- Autocompletion
+				"hrsh7th/nvim-cmp",		-- Required
+				"hrsh7th/cmp-nvim-lsp", -- Required
+				"L3MON4D3/LuaSnip",		-- Required
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-nvim-lua",
+				"octaltree/cmp-look",
+				"hrsh7th/cmp-path",
+				{
+					"dgagn/diagflow.nvim",
+					opts = {scope = "line"}
+				},
+			},
+		},
+		{
+			"j-hui/fidget.nvim",
+			tag = "legacy",
+			opts = {
+				text = { spinner = "moon" },
+				window = { blend = 0 }
+			}
+		},
+		{
+			"scalameta/nvim-metals",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local lsp_zero = require('lsp-zero')
+				local metals_config = require('metals').bare_config()
+				metals_config.capabilities = lsp_zero.get_capabilities()
+				local nvim_metals_group =
+				vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = { "scala", "sbt", "java" },
+					callback = function()
+						require("metals").initialize_or_attach({})
+						local metals_config = require("metals").bare_config()
+						metals_config.settings = {
+							showImplicitArguments = true,
+							serverVersion = "latest.snapshot",
+						}
+						metals_config.init_options.statusBarProvider = "on"
+					end,
+					group = nvim_metals_group,
+				})
+			end
+		},
 
-        -- ui
-        {
-            "folke/todo-comments.nvim",
-            dependencies = { "nvim-lua/plenary.nvim" },
-            event = "VimEnter",
-            opts = {},
-        },
+		-- ui
+		{
+			"folke/todo-comments.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			event = "VimEnter",
+			opts = {},
+		},
 
-        -- lispy stuff (love lisp btw)
-        {"eraserhd/parinfer-rust",
-        build = "cargo build --release",
-        ft = { "lisp", "yuck" }},
-        {"elkowar/yuck.vim",
-        ft = "yuck"},
+		-- lispy stuff (love lisp btw)
+		{"eraserhd/parinfer-rust",
+		build = "cargo build --release",
+		ft = { "lisp", "yuck" }},
+		{"elkowar/yuck.vim",
+		ft = "yuck"},
 
-        -- misc
-        "ThePrimeagen/vim-be-good",
-        "tpope/vim-dispatch",
-        "godlygeek/tabular",
-        "tpope/vim-eunuch",
-    }
+		-- misc
+		"ThePrimeagen/vim-be-good",
+		"tpope/vim-dispatch",
+		"godlygeek/tabular",
+		"tpope/vim-eunuch",
+	}
 })
