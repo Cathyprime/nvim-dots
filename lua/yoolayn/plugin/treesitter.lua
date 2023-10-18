3af7a80f0a5a8d4b5a15789e11631b043dc92ff0
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-18 13:11:30 +0200

    feat(lazy): lazy loading

--- lua/yoolayn/plugin/treesitter.lua
+++ lua/yoolayn/plugin/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	event = {"BufEnter", "BufReadPre"},
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"JoosepAlviste/nvim-ts-context-commentstring",
