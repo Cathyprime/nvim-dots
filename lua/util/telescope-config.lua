15d31882a74b87d0c30137064ee6a61d5f31c07c
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-20 11:07:43 +0200

    feat(telescope): project_files ignore
    without it telescope is absolutely full of unneeded stuff when not using
    git.

--- lua/util/telescope-config.lua
+++ lua/util/telescope-config.lua
@@ -7,18 +7,23 @@ local M = {}
 local is_inside_work_tree = {}
 
 M.project_files = function()
-  local opts = {} -- define here if you want to define something
+  local opts = {
+	  file_ignore_patterns = {
+		  "node%_modules/*",
+		  "venv/*"
+	  }
+  }
 
   local cwd = vim.fn.getcwd()
   if is_inside_work_tree[cwd] == nil then
-	vim.fn.system("git rev-parse --is-inside-work-tree")
-	is_inside_work_tree[cwd] = vim.v.shell_error == 0
+	  vim.fn.system("git rev-parse --is-inside-work-tree")
+	  is_inside_work_tree[cwd] = vim.v.shell_error == 0
   end
 
   if is_inside_work_tree[cwd] then
-	builtin.git_files(opts)
+	  builtin.git_files()
   else
-	builtin.find_files(opts)
+	  builtin.find_files(opts)
   end
 end
 
