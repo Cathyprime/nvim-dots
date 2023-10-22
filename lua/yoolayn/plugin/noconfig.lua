fe719f406f6fbff956acd24a7eef030a6ab7ecd2
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-22 15:21:22 +0200

    feat(lazy): switch to mini.align
    more options basically, and keybinds instead of commands

--- lua/yoolayn/plugin/noconfig.lua
+++ lua/yoolayn/plugin/noconfig.lua
@@ -2,7 +2,6 @@ return {
 	"cohama/lexima.vim",
 	"stefandtw/quickfix-reflector.vim",
 	"kristijanhusak/vim-dadbod-ui",
-	"godlygeek/tabular",
 	"ThePrimeagen/vim-be-good",
 	"anuvyklack/hydra.nvim" ,
 }
