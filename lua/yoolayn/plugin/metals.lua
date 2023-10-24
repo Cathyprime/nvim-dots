83b45dc8f4a23445da86f2a786edbff7e13273bd
Cathyprime <magdajaroszynska47@gmail.com>
2023-10-24 17:17:27 +0200

    fix(scala): scala settings + debugging

--- lua/yoolayn/plugin/metals.lua
+++ lua/yoolayn/plugin/metals.lua
@@ -4,14 +4,39 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-	config = function()
-		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+	config = function(_, _)
+		local metals = require("metals")
+		local metals_config = metals.bare_config()
+		metals_config.settings = {
+			showImplicitArguments = true,
+		}
+		metals_config.init_options.statusBarProvider = "on"
+		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local dap = require("dap")
+		dap.configurations.scala = {
+			{
+				type = "scala",
+				request = "launch",
+				name = "Run with arg and env file",
+				metals = {},
+			}
+		}
+
+		metals_config.on_attach = function(client, bufnr)
+			metals.setup_dap()
+		end
+
+		vim.keymap.set("n", "<leader>lmc", function()
+			require("telescope").extensions.metals.commands()
+		end)
+
+		local nvim_metals_group = vim.api.nvim_create_augroup("metals", { clear = true })
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = { "scala", "sbt", "java" },
 			callback = function()
-				require("metals").initialize_or_attach({})
+				metals.initialize_or_attach(metals_config)
 			end,
 			group = nvim_metals_group,
 		})
-	end
+	end,
 }
