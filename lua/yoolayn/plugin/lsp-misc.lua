a4819e5efef32e608b88800ef0f514d1322a133a
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-26 10:42:14 +0200

    feat(lsp): signature help

--- lua/yoolayn/plugin/lsp-misc.lua
+++ lua/yoolayn/plugin/lsp-misc.lua
@@ -19,4 +19,16 @@ return {
 			},
 		}
 	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {
+			max_height = 6,
+			doc_lines = 6,
+			hint_prefix = ":",
+			hint_enable = false,
+			toggle_key = "<a-s>",
+		},
+		config = function(_, opts) require 'lsp_signature'.setup(opts) end
+	}
 }
