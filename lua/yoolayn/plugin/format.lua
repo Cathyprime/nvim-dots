4652b95130eff2b49724b2360b1d178d353f5eda
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-27 01:01:27 +0200

    feat(lsp): formatters and linters setup

---
+++ lua/yoolayn/plugin/format.lua
@@ -0,0 +1,25 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "gofumpt", "goimports", "golines" },
+				python = { "isort", "black" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+			}
+		})
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end
+}
