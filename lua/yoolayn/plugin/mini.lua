59e64343503243c1354b960e907debec959c88e1
Cathyprime <magdajaroszynska47@gmail.com>
2023-10-26 08:48:38 +0200

    refractor(mini): seperate config for mini plugins

---
+++ lua/yoolayn/plugin/mini.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"echasnovski/mini.indentscope",
+		version = false,
+		opts = {
+			symbol = "│",
+		},
+	},
+	{
+		"echasnovski/mini.align",
+		version = false,
+		opts = {
+			mappings = {
+				start = "",
+				start_with_preview = "ga",
+			},
+		},
+	},
+	{
+		"echasnovski/mini.splitjoin",
+		version = false,
+		opts = {
+			mappings = {
+				toggle = "gs",
+			},
+		},
+	},
+}
--- lua/yoolayn/plugin/misc.lua
+++ lua/yoolayn/plugin/misc.lua
@@ -42,30 +42,4 @@ return {
 			"StartupTime"
 		}
 	},
-	{
-		"echasnovski/mini.indentscope",
-		version = false,
-		opts = {
-			symbol = "│",
-		},
-	},
-	{
-		"echasnovski/mini.align",
-		version = false,
-		opts = {
-			mappings = {
-				start = "",
-				start_with_preview = "ga",
-			},
-		},
-	},
-	{
-		"echasnovski/mini.splitjoin",
-		version = false,
-		opts = {
-			mappings = {
-				toggle = "gs",
-			},
-		},
-	},
 }
