2196167e1fd94b31deb76d48b6f803a319ba5d20
Cathyprime <magdajaroszynska47@gmail.com>
2023-10-26 23:02:33 +0200

    feat(quickfix): trying out bqf instead of reflector

--- lua/yoolayn/plugin/noconfig.lua
+++ lua/yoolayn/plugin/noconfig.lua
@@ -1,7 +1,8 @@
 return {
 	"cohama/lexima.vim",
-	"stefandtw/quickfix-reflector.vim",
+	-- "stefandtw/quickfix-reflector.vim",
 	"kristijanhusak/vim-dadbod-ui",
 	"anuvyklack/hydra.nvim" ,
 	"dohsimpson/vim-macroeditor",
+	"junegunn/fzf.vim"
 }
