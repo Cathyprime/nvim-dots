08989b3d6c50643d7b1b5529da7098ca17dbdd55
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-28 01:24:57 +0200

    feat(lsp): hover info

--- lua/yoolayn/config/lsp.lua
+++ lua/yoolayn/config/lsp.lua
@@ -16,6 +16,7 @@ local function on_attach(client, bufnr)
 	vim.keymap.set("n", "[d",         function() vim.diagnostic.goto_prev() end,   opts)
 	vim.keymap.set("n", "]d",         function() vim.diagnostic.goto_next() end,   opts)
 	vim.keymap.set("n", "gd",         function() vim.lsp.buf.definition() end,     opts)
+	vim.keymap.set("n", "K",          function() vim.lsp.buf.hover() end,          opts)
 
 	vim.api.nvim_buf_set_option(bufnr,"completefunc", "v:lua.vim.lsp.omnifunc")
 	if client.server_capabilities.definitionProvider then
