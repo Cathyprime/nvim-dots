8c976743c645823b82cac5e068e6e05916610508
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-29 00:24:35 +0200

    refractor(cmp): move from lsp file into seperate cmp

--- lua/yoolayn/plugin/lsp.lua
+++ lua/yoolayn/plugin/lsp.lua
@@ -14,17 +14,4 @@ return {
 			require("yoolayn.config.lsp")
 		end,
 	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = { "InsertEnter" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
-			"hrsh7th/cmp-path",
-		},
-		config = function()
-			require("yoolayn.config.cmp")
-			require("yoolayn.config.cmp-colors")
-		end,
-	},
 }
