989bb3ee5d21c1ec4311bf04dd88459b9527e4f4
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-28 21:19:19 +0200

    feat(treesitter): incremental keybinds

--- lua/yoolayn/config/treesitter.lua
+++ lua/yoolayn/config/treesitter.lua
@@ -46,8 +46,18 @@ require("nvim-treesitter.configs").setup({
 	},
 	context_commentstring = { enabled = true },
 	sync_install = false,
-	highlight = { enable = true },
-	additional_vim_regex_highlighting = false,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			node_incremental = "<c-o>",
+			node_decremental = "<c-i>",
+			scope_incremental = "<c-s>",
+		}
+	},
 	textobjects = {
 		select = {
 			enable = true,
@@ -66,28 +76,6 @@ require("nvim-treesitter.configs").setup({
 				["al"] = { query = "@loop.outer", desc = "select inner loop" },
 				["at"] = { query = "@comment.outer", desc = "select a comment" },
 			},
-			-- You can choose the select mode (default is charwise 'v')
-			--
-			-- Can also be a function which gets passed a table with the keys
-			-- * query_string: eg '@function.inner'
-			-- * method: eg 'v' or 'o'
-			-- and should return the mode ('v', 'V', or '<c-v>') or a table
-			-- mapping query_strings to modes.
-			selection_modes = {
-				["@parameter.outer"] = "v", -- charwise
-				["@function.outer"] = "v", -- linewise
-				["@class.outer"] = "<c-v>", -- blockwise
-			},
-			-- If you set this to `true` (default is `false`) then any textobject is
-			-- extended to include preceding or succeeding whitespace. Succeeding
-			-- whitespace has priority in order to act similarly to eg the built-in
-			-- `ap`.
-			--
-			-- Can also be a function which gets passed a table with the keys
-			-- * query_string: eg '@function.inner'
-			-- * selection_mode: eg 'v'
-			-- and should return true of false
-			include_surrounding_whitespace = true,
 		},
 		move = {
 			enable = true,
