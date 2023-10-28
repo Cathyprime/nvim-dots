1b4877230d25da710e17888217edb997e7a30f6e
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-28 18:20:35 +0200

    feat(colorscheme): tokyonight

--- after/plugin/colorscheme.vim
+++ after/plugin/colorscheme.vim
@@ -1,1 +1,2 @@
 colorscheme kanagawa
+" colorscheme tokyonight-storm
---
+++ lua/yoolayn/plugin/tokyonight.lua
@@ -0,0 +1,42 @@
+return {
+	"folke/tokyonight.nvim",
+	config = function()
+		require("tokyonight").setup({
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+			on_highlights = function(hl, c)
+				local prompt = "#2d3149"
+				hl.TelescopeNormal = {
+					bg = c.bg_dark,
+					fg = c.fg_dark,
+				}
+				hl.TelescopeBorder = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopePromptNormal = {
+					bg = prompt,
+				}
+				hl.TelescopePromptBorder = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePromptTitle = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePreviewTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopeResultsTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+			end,
+		})
+	end,
+}
