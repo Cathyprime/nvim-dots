5fa67077e5a79ae9cb4640bc96599e5988fc5429
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-29 01:29:56 +0200

    feat(cmp): has_words_before function, so I don't forget it

--- lua/yoolayn/config/cmp.lua
+++ lua/yoolayn/config/cmp.lua
@@ -5,6 +5,11 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local icons = require("util.icons").icons
 local kind_mapper = require("cmp.types").lsp.CompletionItemKind
 local ts_utils = require("nvim-treesitter.ts_utils")
+-- local has_words_before = function()
+-- 	unpack = unpack or table.unpack
+-- 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+-- 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
 
 vim.api.nvim_create_autocmd({ "InsertEnter", "ColorScheme" }, {
 	callback = function ()
