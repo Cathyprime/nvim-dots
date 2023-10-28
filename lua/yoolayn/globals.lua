28de6babefa412919b6f3f6272a52778545e8291
Cathyprime <magdajaroszynska47@gmail.com>
2023-10-29 00:10:46 +0200

    feat(lua): global helper functions
    stolen from tj deevries

---
+++ lua/yoolayn/globals.lua
@@ -0,0 +1,17 @@
+P = function(v)
+	print(vim.inspect(v))
+	return v
+end
+
+RELOAD = function(...)
+	local ok, plenary_reload = pcall(require, "plenary.reload")
+	if ok then
+		local reloader = plenary_reload.reload_module
+		return reloader(...)
+	end
+end
+
+R = function(name)
+	RELOAD(name)
+	return require(name)
+end
