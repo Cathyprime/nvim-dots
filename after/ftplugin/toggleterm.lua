ee0b740768b01d18b4680f9f5335f72e8519ce70
Cathyprime <magdajaroszynska47@gmail.com>
2023-10-30 16:31:13 +0100

    fix(toggleterm): winbar name

--- after/ftplugin/toggleterm.lua
+++ after/ftplugin/toggleterm.lua
@@ -3,4 +3,4 @@ vim.opt_local.spell = false
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { buffer = 0 })
 vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], { buffer = 0 })
 
-vim.opt_local.winbar = [[%=Terminal%=]]
+vim.opt_local.winbar = [[%===> Terminal <== %=]]
