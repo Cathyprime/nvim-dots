a8bcd8a4a6d299f9fa7e24120533bbeeecd49cbf
Cathyprime <magdajaroszynska47@gmail.com>
2023-10-31 19:45:15 +0100

    feat(luasnip): scala snippets

---
+++ lua/yoolayn/snippets/scala.lua
@@ -0,0 +1,19 @@
+return {
+	s("fn", fmt([[
+	{annotation}def {name}{args}: {retType} = {{
+		{body}
+	}}
+	]], {
+		annotation = c(1, {
+			t"",
+			t({"@annotation.tailrec", ""})
+		}),
+		name = i(2, "name"),
+		args = c(3, {
+			t"",
+			sn(nil, { t"(",i(1, "args") ,t")" }),
+		}),
+		retType = i(4, "Type"),
+		body = i(0)
+	})),
+}
