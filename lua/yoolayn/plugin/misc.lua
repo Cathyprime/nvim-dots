3e4d2badbdeb1f2baef6fad660a6831cff21aea0
Yoolayn <jaroszynskijulian@gmail.com>
2023-10-31 20:38:00 +0100

    feat(git): revamp git integration

--- lua/yoolayn/plugin/misc.lua
+++ lua/yoolayn/plugin/misc.lua
@@ -38,20 +38,56 @@ return {
 		event = {"BufEnter", "BufReadPre"},
 		opts = {
 			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
 				vim.keymap.set(
-				{ "o", "x" },
-				"ih",
-				":<C-u>Gitsigns select_hunk<cr>",
-				{
-					desc = "inner hunk",
-					buffer = bufnr
-				})
+					{ "o", "x" },
+					"ih",
+					"<cmd>Gitsigns select_hunk<cr>",
+					{ desc = "inner hunk", buffer = bufnr}
+				)
+				vim.keymap.set(
+					{ "n" },
+					"<leader>gs",
+					gs.stage_hunk,
+					{ buffer = bufnr }
+				)
+				vim.keymap.set(
+					{ "n" },
+					"<leader>gb",
+					function() gs.blame_line({ full = true }) end,
+					{ buffer = bufnr }
+				)
 			end,
 		}
 	},
 	{
 		"sindrets/diffview.nvim",
-		opts = {},
+		opts = {
+			keymaps = {
+				file_panel = {
+					{
+						"n", "cc",
+						"<Cmd>G commit <bar> wincmd J<CR>",
+						{ desc = "Commit staged changes" },
+					},
+					{
+						"n", "cvc",
+						"<Cmd>G commit <bar> wincmd J<CR>",
+						{ desc = "Commit staged changes" },
+					},
+					{
+						"n", "ca",
+						"<Cmd>G commit --amend <bar> wincmd J<CR>",
+						{ desc = "Amend the last commit" },
+					},
+					{
+						"n", "c<space>",
+						":G commit ",
+						{ desc = "Populate command line with \":G commit \"" },
+					},
+				},
+			}
+		},
 		cmd = {
 			"DiffviewClose",
 			"DiffviewFileHistory",
@@ -60,6 +96,16 @@ return {
 			"DiffviewOpen",
 			"DiffviewRefresh",
 			"DiffviewToggleFiles",
+		},
+		keys = {
+			{
+				"<leader>gd",
+				"<cmd>DiffviewOpen<cr>"
+			},
+			{
+				"<leader>gc",
+				"<cmd>DiffviewClose<cr>"
+			}
 		}
 	},
 	{
