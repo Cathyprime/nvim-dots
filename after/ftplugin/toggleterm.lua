43955f97c8e70cdc25d410466707dea7012ff6d1
Cathyprime <magdajaroszynska47@gmail.com>
2023-11-04 15:52:55 +0100

    fix(linenum): disable where it shouldn't be

--- after/ftplugin/toggleterm.lua
+++ after/ftplugin/toggleterm.lua
@@ -3,3 +3,5 @@ vim.opt_local.spell = false
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { buffer = 0 })
 
 vim.opt_local.winbar = [[%= ==> Terminal <== %=]]
+vim.opt_local.relativenumber = false
+vim.opt_local.number = false
