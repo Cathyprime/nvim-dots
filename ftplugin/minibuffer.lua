afb74572158d1cfff267ef458eb27f7ea95b0d76
Cathyprime <magdajaroszynska47@gmail.com>
2023-11-09 00:40:11 +0100

    feat(minibuffer): move to ftplugin

---
+++ ftplugin/minibuffer.lua
@@ -0,0 +1,30 @@
+vim.keymap.set({ "n", "i"}, "<c-g>", "norm :close<cr>", { buffer = true, silent = true })
+vim.keymap.set({ "n", "i"}, "<c-c>", "<cmd>close<cr>", { buffer = true, silent = true })
+vim.keymap.set({"i"}, "<c-k>", "<up>", { buffer = true, silent = true })
+vim.keymap.set({"i"}, "<c-j>", "<down>", { buffer = true, silent = true })
+vim.api.nvim_win_set_height(0, 1)
+vim.opt_local.spell = false
+vim.opt_local.pumheight = 3
+vim.opt_local.winbar = nil
+vim.o.laststatus = 0
+
+local cmp = require("cmp")
+---@diagnostic disable-next-line
+cmp.setup.buffer({
+	sources = cmp.config.sources({
+		{ name = "path" },
+		{ name = "cmdline" },
+	}, {
+		{ name = "buffer" },
+	}, {
+		{ name = "nvim_lua" },
+	}),
+
+})
+
+vim.api.nvim_create_autocmd("CmdwinLeave", {
+	once = false,
+	callback = function()
+		vim.o.laststatus = 3
+	end
+})
