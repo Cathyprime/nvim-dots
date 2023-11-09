fb1782238c0bf8b27db7de3c987c7bb4bc9de0c1
Yoolayn <jaroszynskijulian@gmail.com>
2023-11-09 01:11:28 +0100

    feat(minibuffer): supertab, and start in insert mode again

--- ftplugin/minibuffer.lua
+++ ftplugin/minibuffer.lua
@@ -18,6 +18,13 @@ vim.api.nvim_create_autocmd("CmdwinLeave", {
 	end
 })
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
 local cmp = require("cmp")
 ---@diagnostic disable-next-line
 cmp.setup.buffer({
@@ -30,4 +37,28 @@ cmp.setup.buffer({
 		{ name = "nvim_lua" },
 	}),
 
+	mapping = {
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+  },
 })
