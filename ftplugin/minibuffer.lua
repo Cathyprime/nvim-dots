542a05f0f21d80fe673e3058624d93d6dc6184b9
Yoolayn <jaroszynskijulian@gmail.com>
2023-11-09 01:05:17 +0100

    fix(minibuffer): restore pumheight

--- ftplugin/minibuffer.lua
+++ ftplugin/minibuffer.lua
@@ -4,9 +4,19 @@ vim.keymap.set({"i"}, "<c-k>", "<up>", { buffer = true, silent = true })
 vim.keymap.set({"i"}, "<c-j>", "<down>", { buffer = true, silent = true })
 vim.api.nvim_win_set_height(0, 1)
 vim.opt_local.spell = false
-vim.opt_local.pumheight = 3
 vim.opt_local.winbar = nil
 vim.o.laststatus = 0
+
+local old_height = vim.opt.pumheight
+vim.opt.pumheight = 3
+
+vim.api.nvim_create_autocmd("CmdwinLeave", {
+	once = false,
+	callback = function()
+		vim.o.laststatus = 3
+		vim.opt.pumheight = old_height
+	end
+})
 
 local cmp = require("cmp")
 ---@diagnostic disable-next-line
@@ -20,11 +30,4 @@ cmp.setup.buffer({
 		{ name = "nvim_lua" },
 	}),
 
-})
-
-vim.api.nvim_create_autocmd("CmdwinLeave", {
-	once = false,
-	callback = function()
-		vim.o.laststatus = 3
-	end
 })
