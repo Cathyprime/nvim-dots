2fb542ab8f297adfe05152413967b21b4b34c10b
Cathyprime <magdajaroszynska47@gmail.com>
2023-11-12 20:01:02 +0100

    feat(luasnip): go snippets
    Nicer fn snippet, method snippet

--- lua/yoolayn/snippets/go.lua
+++ lua/yoolayn/snippets/go.lua
@@ -12,12 +12,61 @@ return {
 	})),
 
 	s("fn", fmt([[
-	func {name}({args}) {{
+	func{space}{name}({args}) {left}{return_val}{right}{{
+		{body}
+	}}
+	]], {
+		space = f(function(name)
+			if name[1][1] == "" then
+				return ""
+			else
+				return " "
+			end
+		end, { 1 }),
+		name = i(1),
+		args = i(2),
+		return_val = i(3),
+		body = i(0),
+		left = f(function(values)
+			if values[1][1] == "" then
+				return ""
+			else
+				return "("
+			end
+		end, { 3 }),
+		right = f(function(values)
+			if values[1][1] == "" then
+				return ""
+			else
+				return ") "
+			end
+		end, { 3 }),
+	})),
+
+	s("m", fmt([[
+	func ({receiver}) {name}({args}) {left}{return_val}{right}{{
 		{body}
 	}}
 	]], {
 		name = i(1, "name"),
 		args = i(2),
+		receiver = i(3),
+		return_val = i(4),
+		left = f(function(name)
+			if name[1][1] == "" then
+				return ""
+			else
+				return "("
+			end
+		end, { 4 }),
+		right = f(function(name)
+			if name[1][1] == "" then
+				return ""
+			else
+				return ") "
+			end
+		end, { 4 }),
 		body = i(0),
 	})),
+
 }
