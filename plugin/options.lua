7a94429c33a9a0fdde585665b405d0577167006b
Yoolayn <jaroszynskijulian@gmail.com>
2023-11-14 12:37:45 +0100

    feat(options): merge files together

--- plugin/options.lua
+++ plugin/options.lua
@@ -1,4 +1,65 @@
-local options = require("yoolayn.settings.options")
+local options = {}
+
+options.options = {
+	rnu  =  true,
+	nu  =  true,
+	spell  =  true,
+	spl = "en_us,en_gb,pl",
+	tabstop = 4,
+	shiftwidth = 4,
+	smartindent = true,
+	exrc = true,
+	list = true,
+	listchars = {
+		append = "trail:-,tab:\\u0020\\u0020",
+	},
+	path = ".,**",
+	ignorecase = false,
+	smartcase = true,
+	incsearch = true,
+	hls = true,
+	cursorline = true,
+	guicursor = "i-ci-ve:block",
+	showtabline = 0,
+	scrolloff = 8,
+	termguicolors = true,
+	signcolumn = "yes",
+	inccommand = "split",
+	splitright = true,
+	splitbelow = true,
+	timeoutlen = 10000,
+	updatetime = 50,
+	swapfile = false,
+	writebackup = false,
+	shortmess = {
+		append = "c",
+	},
+	showmode = false,
+	laststatus = 3,
+	undofile = true,
+	undodir = os.getenv("HOME") .. "/.config/nvim/undo",
+	wildmode = "longest:full,full",
+	completeopt = "menu,noselect",
+	winminwidth = 5,
+	pumheight = 6,
+	wrap = false,
+}
+
+options.prg = {
+	grep = "rg --vimgrep --no-heading --smart-case",
+}
+
+options.globals = {
+	markdown_recommended_style = 0,
+	loaded_netrw = 1,
+	loaded_netrwPlugin = 1,
+}
+
+
+
+
+
+
 
 local function set_option(name, opts)
 	local obj = vim.opt[name]
