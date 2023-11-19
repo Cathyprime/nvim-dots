adbea1a22b8567e4755d8016aff82e0a5a1ec7d2
Yoolayn <jaroszynskijulian@gmail.com>
2023-11-19 14:25:36 +0100

    feat(lsp): lsp lines stay on

--- lua/yoolayn/plugin/lsp-misc.lua
+++ lua/yoolayn/plugin/lsp-misc.lua
@@ -31,9 +31,7 @@ return {
 			require("lsp_lines").setup()
 			vim.diagnostic.config({
 				virtual_text = false,
-				virtual_lines = {
-					only_current_line = true
-				}
+				virtual_lines = true
 			})
 		end,
 	}
