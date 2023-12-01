991e13a20b79f337af845dc7ac14e59c709a0fd0
Cathyprime <magdajaroszynska47@gmail.com>
2023-12-01 10:06:03 +0100

    feat(lazy): harpoon!!

--- lua/yoolayn/plugin/misc.lua
+++ lua/yoolayn/plugin/misc.lua
@@ -93,4 +93,21 @@ return {
 			require('fundo').install()
 		end,
 	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup({})
+			local function map(lhs, rhs, opts)
+				vim.keymap.set("n", lhs, rhs, opts or {})
+			end
+			map("<leader>a", function() harpoon:list():append() end)
+			map("<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+			map("<c-h>h", function() harpoon:list():select(1) end)
+			map("<c-h>j", function() harpoon:list():select(2) end)
+			map("<c-h>k", function() harpoon:list():select(3) end)
+			map("<c-h>l", function() harpoon:list():select(4) end)
+		end
+	}
 }
