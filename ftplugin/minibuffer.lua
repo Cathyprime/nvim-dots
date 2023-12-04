5441a64d7f82359b15e798f8a9f45e9daa710412
Cathyprime <magdajaroszynska47@gmail.com>
2023-12-04 01:10:53 +0100

    style(minibuffer): spacing

--- ftplugin/minibuffer.lua
+++ ftplugin/minibuffer.lua
@@ -41,9 +41,9 @@ vim.api.nvim_create_autocmd("CmdwinLeave", {
 })
 
 local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local luasnip = require("luasnip")
