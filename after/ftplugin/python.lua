fc64016915f9e138eeb28b9abb331354634c1fa6
Cathyprime <magdajaroszynska47@gmail.com>
2023-12-05 15:08:42 +0100

    feat(mini): indentscope only show on python files

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,3 +1,10 @@
 vim.b.dispatch = "python %"
 vim.keymap.set("n", "<localleader>r", "<cmd>Dispatch<cr>", { silent = true })
 vim.opt_local.listchars:remove([[leadmultispace]])
+
+local ok, _ = pcall(require, "mini.indentscope")
+if ok then
+	vim.b.miniindentscope_config = {
+		symbol = "│"
+	}
+end
