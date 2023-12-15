fdd12f8413ab40a6092fb3cafb697b265f8355ab
Cathyprime <magdajaroszynska47@gmail.com>
2023-12-15 16:28:30 +0100

    remove: Harpoon

--- lua/yoolayn/plugin/misc.lua
+++ lua/yoolayn/plugin/misc.lua
@@ -62,24 +62,6 @@ return {
 		end,
 	},
 	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		config = function()
-			local harpoon = require("harpoon")
-			---@diagnostic disable-next-line: missing-parameter
-			harpoon:setup()
-			local function map(lhs, rhs, opts)
-				vim.keymap.set("n", lhs, rhs, opts or {})
-			end
-			map("<leader>a", function() harpoon:list():append() end)
-			map("<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-			map("<c-h><c-h>", function() harpoon:list():select(1) end)
-			map("<c-h><c-j>", function() harpoon:list():select(2) end)
-			map("<c-h><c-k>", function() harpoon:list():select(3) end)
-			map("<c-h><c-l>", function() harpoon:list():select(4) end)
-		end
-	},
-	{
 		"stevearc/dressing.nvim",
 		opts = {}
 	},
