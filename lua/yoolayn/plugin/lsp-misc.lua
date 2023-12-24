d8c766054f4574626d83d0383f4fb44254366838
Yoolayn <jaroszynskijulian@gmail.com>
2023-12-24 15:44:39 +0100

    remove(lsp): diagflow

--- lua/yoolayn/plugin/lsp-misc.lua
+++ lua/yoolayn/plugin/lsp-misc.lua
@@ -18,35 +18,4 @@ return {
 			}
 		}
 	},
-	{
-		"dgagn/diagflow.nvim",
-		lazy = true,
-		init = function()
-			vim.keymap.set("n", "<leader>td", require("diagflow").toggle)
-		end,
-		opts = {
-			max_width = 80,
-			scope = "line",
-			padding_top = 2,
-			padding_right = 6,
-			show_sign = false,
-			toggle_event = { "InsertEnter", "InsertLeave" },
-			severity_colors = {
-				error = "DiagnosticError"
-			},
-			format = function(diagnostic)
-				local icons = require("util.icons").icons
-				local severity = {
-					icons.Error,
-					icons.Warning,
-					icons.Info,
-					icons.Hint,
-				}
-				return severity[diagnostic.severity or 3] .. " " .. (diagnostic.message or "haii :3")
-			end,
-			enable = function()
-				return vim.bo.filetype ~= "lazy"
-			end
-		}
-	}
 }
