f94be7251203ea98c0d7a17c0109bacc03513466
Cathyprime <magdajaroszynska47@gmail.com>
2023-12-24 19:21:26 +0100

    feat(ftplugin): (java/type)scriptreact indent lines

--- after/ftplugin/javascriptreact.lua
+++ after/ftplugin/javascriptreact.lua
@@ -1,3 +1,9 @@
 vim.opt_local.expandtab = false
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
+local ok, _ = pcall(require, "mini.indentscope")
+if ok then
+	vim.b.miniindentscope_config = {
+		symbol = "│"
+	}
+end
