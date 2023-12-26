9fbaa82d97023e77baa035a936867435d6d99676
Cathyprime <magdajaroszynska47@gmail.com>
2023-12-26 14:28:38 +0100

    feat(neorg): looking glass keymap

--- lua/yoolayn/plugin/neorg.lua
+++ lua/yoolayn/plugin/neorg.lua
@@ -13,6 +13,13 @@ return {
 						}
 					}
 				},
+				["core.keybinds"] = {
+					config = {
+						hook = function(keybinds)
+							keybinds.map("norg", "n", "gf", "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<cr>")
+						end
+					}
+				},
 				["core.export"] = { config = { export_dir = "~/Documents/neorg/exported" } },
 				["core.ui.calendar"] = {},
 				["core.dirman"] = {
