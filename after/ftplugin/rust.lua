be143ce3218f2b6a89d3acd326e27f81294c9614
Yoolayn <jaroszynskijulian@gmail.com>
2023-12-26 22:36:35 +0100

    feat(ftplugin): rust

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,4 +1,6 @@
 vim.cmd"compiler cargo"
-vim.b["dispatch"] = "cargo run"
-
+vim.b["dispatch"] = "rustc %"
+vim.opt_local.expandtab = true
 vim.opt_local.include = [[\\v^\\s*(pub\\s+)?use\\s+\\zs(\\f\|:)+]]
+vim.opt_local.listchars:remove "leadmultispace"
+vim.opt_local.listchars:append [[tab:\u00bb\u0020]]
