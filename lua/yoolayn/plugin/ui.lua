0b2553eac7b048bb32d415ca426d1e728aea0d26
Yoolayn <jaroszynskijulian@gmail.com>
2024-01-05 20:27:22 +0100

    feat(telescope): remove duplicated code

--- lua/yoolayn/plugin/ui.lua
+++ lua/yoolayn/plugin/ui.lua
@@ -1,3 +1,5 @@
+local telescope_config = require("util.telescope-config")
+
 return {
     {
         "stevearc/dressing.nvim",
@@ -6,17 +8,9 @@ return {
                 telescope = (function()
                     local ivy = require("telescope.themes").get_ivy()
                     local settings = {
-                        borderchars = {
-                            prompt  = { "",  "",  "",  "",  "",  "",  "",  ""  },
-                            results = { "",  "",  "",  "",  "",  "",  "",  ""  },
-                            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-                        },
-                        layout_config = {
-                            prompt_position = "bottom",
-                            height = 14,
-                            preview_width = 0.60,
-                        },
-                        border = true,
+                        borderchars = telescope_config.borderchars,
+                        layout_config = telescope_config.layout_config,
+                        border = telescope_config.border,
                     }
                     return vim.tbl_deep_extend("force", ivy, settings)
                 end)()
