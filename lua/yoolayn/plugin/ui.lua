61e8584b6b485fa1de8d4600d1f144c1033c9349
Yoolayn <jaroszynskijulian@gmail.com>
2024-01-05 20:14:30 +0100

    feat(ui): fix dressing telescope config

--- lua/yoolayn/plugin/ui.lua
+++ lua/yoolayn/plugin/ui.lua
@@ -1,7 +1,27 @@
 return {
     {
         "stevearc/dressing.nvim",
-        opts = {}
+        opts = {
+            select = {
+                telescope = (function()
+                    local ivy = require("telescope.themes").get_ivy()
+                    local settings = {
+                        borderchars = {
+                            prompt  = { "",  "",  "",  "",  "",  "",  "",  ""  },
+                            results = { "",  "",  "",  "",  "",  "",  "",  ""  },
+                            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                        },
+                        layout_config = {
+                            prompt_position = "bottom",
+                            height = 14,
+                            preview_width = 0.60,
+                        },
+                        border = true,
+                    }
+                    return vim.tbl_deep_extend("force", ivy, settings)
+                end)()
+            }
+        }
     },
     {
         "nvim-lualine/lualine.nvim",
