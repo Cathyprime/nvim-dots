7ab2eebbc650fd34f18bb56bc44a6a784b2d1ef8
Cathyprime <magdajaroszynska47@gmail.com>
2024-01-07 21:51:18 +0100

    fix(nonels): styling -> styleline

--- lua/yoolayn/plugin/nonels.lua
+++ lua/yoolayn/plugin/nonels.lua
@@ -4,17 +4,18 @@ return {
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.black,
-                null_ls.builtins.formatting.prettierd,
                 null_ls.builtins.formatting.eslint_d,
+                null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.formatting.prettierd,
+                null_ls.builtins.formatting.stylua,
 
-                null_ls.builtins.diagnostics.luacheck,
+                null_ls.builtins.diagnostics.eslint_d,
                 null_ls.builtins.diagnostics.golangci_lint,
+                null_ls.builtins.diagnostics.luacheck,
                 null_ls.builtins.diagnostics.mypy,
                 null_ls.builtins.diagnostics.ruff,
-                null_ls.builtins.diagnostics.eslint_d,
+                null_ls.builtins.diagnostics.stylelint,
 
                 null_ls.builtins.code_actions.eslint_d,
             },
