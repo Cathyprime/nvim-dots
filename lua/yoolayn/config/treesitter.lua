1e843bb947a42ed7561037fbdd0e725909deab1e
Yoolayn <jaroszynskijulian@gmail.com>
2024-01-07 11:51:04 +0100

    feat(treesitter): indent

--- lua/yoolayn/config/treesitter.lua
+++ lua/yoolayn/config/treesitter.lua
@@ -7,6 +7,9 @@ require("nvim-treesitter.configs").setup({
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+    indent = {
+        enable = true,
+    },
     incremental_selection = {
         enable = true,
         keymaps = {
