2c079fd708983df0a702407db0ed1c7e3e76df96
Cathyprime <magdajaroszynska47@gmail.com>
2024-01-14 14:48:10 +0100

    feat(ftplugin): remove all the weird jumping with tab/leadmultispace

--- after/ftplugin/javascriptreact.lua
+++ after/ftplugin/javascriptreact.lua
@@ -1,9 +1,9 @@
 vim.opt_local.expandtab = true
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
-vim.opt_local.listchars:remove "tab"
-vim.opt_local.listchars:append [[tab:\u00b7\u0020\u0020]]
-vim.opt_local.listchars:append [[leadmultispace:\u0020\u0020\u0020]]
+-- vim.opt_local.listchars:remove "tab"
+-- vim.opt_local.listchars:append [[tab:\u00b7\u0020\u0020]]
+-- vim.opt_local.listchars:append [[leadmultispace:\u0020\u0020\u0020]]
 local ok, _ = pcall(require, "mini.indentscope")
 if ok then
     vim.b.miniindentscope_config = {
