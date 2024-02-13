250ecebbe28a48908327c749e105873aee0b48d9
Cathyprime <magdajaroszynska47@gmail.com>
2024-02-13 20:03:44 +0100

    feat(highlights): highlight trailing white space

--- after/plugin/highlights.lua
+++ after/plugin/highlights.lua
@@ -8,10 +8,38 @@ local diagGroups = {
     "DiagnosticFloatingInfo"
 }
 
+local function get_id()
+    for _, match in ipairs(vim.fn.getmatches()) do
+        if match.group == "TrailingWhitespace" then return match.id end
+    end
+end
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+    once = false,
+    callback = function()
+        pcall(vim.fn.matchdelete, get_id())
+    end
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+    once = false,
+    callback = function()
+        if get_id() ~= nil then return end
+        vim.fn.matchadd("TrailingWhitespace", [[\s\+$]])
+    end
+})
+
 vim.api.nvim_create_autocmd({"ColorScheme", "VimEnter"}, {
     once = false,
     group = augroup,
     callback = function()
+        vim.api.nvim_set_hl(0, "TrailingWhitespace", {
+            bg = (function()
+                local color = vim.api.nvim_get_hl(0, { name = "@keyword.return" })
+                print(vim.inspect(color))
+                return color.fg
+            end)()
+        })
         vim.api.nvim_set_hl(0, "PortalOrange", {
             fg = "#fd6600"
         })
