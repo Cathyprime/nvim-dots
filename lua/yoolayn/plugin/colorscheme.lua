bbbb9f3d2cf0e13e0d258ba5ed9cbce7963674ba
Cathyprime <magdajaroszynska47@gmail.com>
2024-02-18 16:33:48 +0100

    style(colorscheme): consistent tables

--- lua/yoolayn/plugin/colorscheme.lua
+++ lua/yoolayn/plugin/colorscheme.lua
@@ -12,31 +12,12 @@ require("mini.deps").now(function()
             local theme = colors.theme
             return {
                 Normal = { bg = "#16161d" },
-                Whitespace = {
-                    bg = "none",
-                    fg = "#1e1e28"
-                },
-                TelescopeTitle = {
-                    fg = theme.ui.special,
-                    bg = theme.ui.bg_p1,
-                    bold = true,
-                },
-                TelescopePromptBorder = {
-                    fg = theme.ui.bg_p1,
-                    bg = "NONE",
-                },
-                TelescopeResultsNormal = {
-                    fg = theme.ui.fg_dim,
-                    bg = "NONE",
-                },
-                TelescopeResultsBorder = {
-                    fg = theme.ui.bg_m1,
-                    bg = "NONE",
-                },
-                TelescopePreviewBorder = {
-                    fg = theme.ui.bg_dim,
-                    bg = "NONE",
-                },
+                Whitespace = { bg = "none", fg = "#1e1e28" },
+                TelescopeTitle = { fg = theme.ui.special, bg = theme.ui.bg_p1, bold = true, },
+                TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = "NONE", },
+                TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = "NONE", },
+                TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = "NONE", },
+                TelescopePreviewBorder = { fg = theme.ui.bg_dim, bg = "NONE", },
                 Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
                 PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
                 PmenuSbar = { bg = theme.ui.bg_m1 },
