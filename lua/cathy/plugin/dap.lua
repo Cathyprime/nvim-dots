f60de1140f586ca183c8cff4c685f7c8a7be8c42
Cathyprime <magdajaroszynska47@gmail.com>
2024-03-09 15:33:56 +0100

    fix(dap): toggle breakpoint if adapter is setup

--- lua/cathy/plugin/dap.lua
+++ lua/cathy/plugin/dap.lua
@@ -2,12 +2,33 @@
 require("mini.deps").add("mfussenegger/nvim-dap")
 require("mini.deps").add("rcarriga/nvim-dap-ui")
 
+local dap_ft = {
+    "go",
+}
+
+local function isSetup(ft)
+    if vim.b["dap_on"] == nil then
+        vim.b["dap_on"] = vim.iter(dap_ft):any(function(v)
+            return ft == v
+        end)
+    end
+    return vim.b["dap_on"]
+end
+
+local enter = vim.api.nvim_replace_termcodes("norm! <cr>", true, true, true)
+
 local function map(lhs, rhs, description, skip)
     local prefix = "<leader>z"
     if skip ~= nil then
         prefix = ""
     end
-    vim.keymap.set("n", prefix .. lhs, rhs, { desc = description })
+    vim.keymap.set("n", prefix .. lhs, function()
+        if isSetup(vim.o.filetype) then
+            rhs()
+        else
+            vim.cmd(enter)
+        end
+    end, { desc = description })
 end
 
 require("mini.deps").later(function()
