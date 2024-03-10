1af0185b3d12eb4381345e9b903ddb966419a588
Cathyprime <magdajaroszynska47@gmail.com>
2024-03-10 20:51:30 +0100

    feat(mini): mini.ai

--- lua/cathy/plugin/mini.lua
+++ lua/cathy/plugin/mini.lua
@@ -83,4 +83,18 @@ later(function()
             toggle = "gs",
         }
     })
+
+    local ai = require("mini.ai")
+    ai.setup({
+        custom_textobjects = {
+            o = ai.gen_spec.treesitter({
+                a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+                i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+            }, {}),
+            f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+            c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+        },
+        search_method = "cover_or_prev"
+    })
+
 end)
