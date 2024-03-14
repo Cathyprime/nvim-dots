b6dd4c693db21ef10081b6a560bf837fe53d510d
Cathyprime <magdajaroszynska47@gmail.com>
2024-03-14 02:43:47 +0100

    feat(ftplugin): go ro format options

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,3 +1,11 @@
 vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 8
 vim.opt_local.tabstop = 8
+-- vim.opt.indentkeys:remove("<:>")
+vim.api.nvim_create_autocmd("BufEnter", {
+    once = true,
+    callback = function()
+        vim.opt.indentkeys:remove("<:>")
+        vim.opt_local.formatoptions:append("ro")
+    end
+})
