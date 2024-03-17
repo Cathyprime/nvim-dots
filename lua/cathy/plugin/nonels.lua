0863e3f5beebb2dd108ff5213daab18a4f4fcbaf
Cathyprime <magdajaroszynska47@gmail.com>
2024-03-17 16:30:00 +0100

    feat(none): java formatter

--- lua/cathy/plugin/nonels.lua
+++ lua/cathy/plugin/nonels.lua
@@ -9,6 +9,7 @@ require("mini.deps").later(function()
             null_ls.builtins.formatting.prettierd,
             null_ls.builtins.formatting.stylua,
             null_ls.builtins.formatting.black,
+            null_ls.builtins.formatting.google_java_format,
 
             null_ls.builtins.diagnostics.golangci_lint,
             null_ls.builtins.diagnostics.stylelint,
