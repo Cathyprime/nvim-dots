9454e85995f5479f86168ebe7bd2c8aa471a430f
Cathyprime <magdajaroszynska47@gmail.com>
2024-03-17 20:03:12 +0100

    fix(lsp): disable error

--- lua/cathy/config/lsp.lua
+++ lua/cathy/config/lsp.lua
@@ -25,6 +25,7 @@ local border = {
 }
 
 local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+---@diagnostic disable-next-line
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
     opts = opts or {}
     opts.border = opts.border or border
