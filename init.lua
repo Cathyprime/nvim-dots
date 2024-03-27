7747892b4e90d1751393ed17637851aedb73cf11
Cathyprime <magdajaroszynska47@gmail.com>
2024-03-27 19:12:01 +0100

    feat(init): rocks pkg manager

--- init.lua
+++ init.lua
@@ -1,6 +1,41 @@
 vim.g.mapleader = " "
 vim.g.localleader = "\\"
 
+if vim.fn.executable("luarocks") == 1 then
+    local rocks_config = {
+        rocks_path = vim.fn.stdpath("data") .. "/rocks",
+        luarocks_binary = "luarocks",
+    }
+
+    vim.g.rocks_nvim = rocks_config
+
+    local luarocks_path = {
+        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+        vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+    }
+    package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+    local luarocks_cpath = {
+        vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+        vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+    }
+    package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+    vim.opt.runtimepath:append(vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*"))
+
+    if not vim.fn.exists("Rocks") then
+        vim.system({
+            "luarocks",
+            "--lua-version=5.1",
+            "--tree",
+            vim.g.rocks_nvim.rocks_path,
+            "--server='https://nvim-neorocks.github.io/rocks-binaries/'",
+            "install",
+            "rocks.nvim"
+        })
+    end
+end
+
 SWITCHES = {
     files = "oil",
     scala = true,
