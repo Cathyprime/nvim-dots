082d3f94be85416aae2682ab460df3f44598297a
Cathyprime <magdajaroszynska47@gmail.com>
2024-04-05 16:03:10 +0200

    feat: move lspconfig to deps

--- lua/cathy/plugin/lsp.lua
+++ lua/cathy/plugin/lsp.lua
@@ -1,3 +1,11 @@
+require("mini.deps").add({
+    source = "williamboman/mason-lspconfig.nvim",
+    depends = {
+        "williamboman/mason.nvim",
+        "neovim/nvim-lspconfig",
+    },
+})
+
 require("mini.deps").later(function()
     require("cathy.config.lsp")
 end)
