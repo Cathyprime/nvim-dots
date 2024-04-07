f572c652f72901569abf4a903cd251bde4dd6d91
Cathyprime <magdajaroszynska47@gmail.com>
2024-04-07 10:18:42 +0200

    style: change string quotes

--- lua/cathy/minideps.lua
+++ lua/cathy/minideps.lua
@@ -3,17 +3,17 @@ local mini_path = path_package .. '/pack/deps/start/mini.nvim'
 if not vim.loop.fs_stat(mini_path) then
     vim.cmd('echo "Installing `mini.nvim`" | redraw')
     local clone_cmd = {
-        'git', 'clone', '--filter=blob:none',
+        "git", "clone", "--filter=blob:none",
         -- Uncomment next line to use 'stable' branch
         -- '--branch', 'stable',
-        'https://github.com/echasnovski/mini.nvim', mini_path
+        "https://github.com/echasnovski/mini.nvim", mini_path
     }
     vim.fn.system(clone_cmd)
-    vim.cmd('packadd mini.nvim | helptags ALL')
+    vim.cmd("packadd mini.nvim | helptags ALL")
 end
 
 -- Set up 'mini.deps' (customize to your liking)
-require('mini.deps').setup({ path = { package = path_package } })
+require("mini.deps").setup({ path = { package = path_package } })
 
 require("cathy.plugin.mini")
 require("cathy.plugin.rooter")
