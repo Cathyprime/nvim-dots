4b535b194d03fb4f7fdfa64b20599c25afd7eaaa
Cathyprime <magdajaroszynska47@gmail.com>
2024-04-24 23:38:59 +0200

    fix(none): eslint + prettier setup

--- lua/cathy/plugin/nonels.lua
+++ lua/cathy/plugin/nonels.lua
@@ -6,13 +6,14 @@ require("mini.deps").later(function()
         sources = {
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.gofumpt,
-            null_ls.builtins.formatting.prettierd,
+            null_ls.builtins.formatting.prettier,
             null_ls.builtins.formatting.stylua,
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.google_java_format,
 
             null_ls.builtins.diagnostics.golangci_lint,
             null_ls.builtins.diagnostics.stylelint,
+            require("none-ls.diagnostics.eslint_d"),
         },
     })
     vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
