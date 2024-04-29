6cf11053c8981831405b21b0dee0bbf3a3a79d51
Cathyprime <magdajaroszynska47@gmail.com>
2024-04-29 09:01:51 +0200

    style(ftplugin): rust

--- lua/cathy/config/rust.lua
+++ lua/cathy/config/rust.lua
@@ -1,16 +1,17 @@
 vim.g.rustaceanvim = {
-    server = { on_attach = function(client, bufnr)
+    server = {
+        on_attach = function(client, bufnr)
             require("cathy.config.lsp-funcs").on_attach(client, bufnr, {
                 code_action = function()
                     vim.cmd.RustLsp("codeAction")
                 end,
                 hover = function()
-                    vim.cmd.RustLsp({"hover", "range"})
-                    vim.cmd.RustLsp({"hover", "actions"})
-                end
+                    vim.cmd.RustLsp({ "hover", "range" })
+                    vim.cmd.RustLsp({ "hover", "actions" })
+                end,
             })
-        end
-    }
+        end,
+    },
 }
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufEnterPre" }, {
