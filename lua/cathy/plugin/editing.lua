66b357979375e59a07272c891ed52e7c42c0647d
Cathyprime <magdajaroszynska47@gmail.com>
2024-05-09 00:34:06 +0200

    feat(editing): dial customizations

--- lua/cathy/plugin/editing.lua
+++ lua/cathy/plugin/editing.lua
@@ -1,4 +1,4 @@
-local add   = require("mini.deps").add
+local add = require("mini.deps").add
 local later = require("mini.deps").later
 
 add("kylechui/nvim-surround")
@@ -7,7 +7,7 @@ later(function()
     vim.api.nvim_create_autocmd("InsertEnter", {
         once = true,
         callback = function()
-        ---@diagnostic disable-next-line
+            ---@diagnostic disable-next-line
             require("nvim-surround").setup({
                 keymaps = {
                     insert = "<c-d>",
@@ -21,7 +21,7 @@ later(function()
                     delete = "ds",
                     change = "cs",
                     change_line = "cS",
-                }
+                },
             })
         end,
     })
@@ -29,7 +29,7 @@ end)
 
 add({
     source = "windwp/nvim-autopairs",
-    depends = { "hrsh7th/nvim-cmp", }
+    depends = { "hrsh7th/nvim-cmp" },
 })
 later(function()
     vim.api.nvim_create_autocmd("InsertEnter", {
@@ -38,10 +38,10 @@ later(function()
             require("nvim-autopairs").setup()
             require("nvim-autopairs").remove_rule('"')
             require("nvim-autopairs").remove_rule("'")
-            require("nvim-autopairs").remove_rule('`')
-            require("nvim-autopairs").remove_rule('(')
-            require("nvim-autopairs").remove_rule('[')
-            require("nvim-autopairs").remove_rule('{')
+            require("nvim-autopairs").remove_rule("`")
+            require("nvim-autopairs").remove_rule("(")
+            require("nvim-autopairs").remove_rule("[")
+            require("nvim-autopairs").remove_rule("{")
             local ok, _ = pcall(require, "cmp")
             if ok then
                 require("cathy.config.cmp-pairs")
@@ -57,54 +57,102 @@ later(function()
         once = true,
         callback = function()
             local augend = require("dial.augend")
-            require("dial.config").augends:register_group({
-                default = {
+            local function extend(t)
+                local defaults = {
                     augend.integer.alias.binary,
-                    augend.integer.alias.decimal,
+                    augend.integer.alias.decimal_int,
                     augend.integer.alias.hex,
-                    augend.constant.alias.bool,
-                    augend.case.new{
-                        types = {
-                            "camelCase",
-                            "PascalCase",
-                            "snake_case",
-                            "SCREAMING_SNAKE_CASE",
-                        },
-                        cyclic = true,
-                    },
                     augend.date.alias["%d/%m/%Y"],
                     augend.constant.new({ elements = { "&&", "||" }, word = false, cyclic = true }),
                     augend.constant.new({ elements = { "and", "or" } }),
+                    augend.constant.new({
+                        elements = { "true", "false" },
+                        word = true,
+                        cyclic = true,
+                        preserve_case = true,
+                    }),
+                    augend.decimal_fraction.new({
+                        signed = true,
+                        point_char = ".",
+                    })
                 }
-            })
-            require("dial.config").augends:on_filetype({
-                python = {
-                    augend.integer.alias.binary,
-                    augend.integer.alias.decimal,
-                    augend.integer.alias.hex,
-                    augend.constant.new({ elements = { "True", "False" } }),
-                    augend.case.new{
+                for _, v in ipairs(t or {}) do
+                    table.insert(defaults, v)
+                end
+                return defaults
+            end
+
+            require("dial.config").augends:register_group({
+                default = extend(),
+                case = {
+                    augend.case.new({
                         types = {
                             "camelCase",
                             "PascalCase",
+                            "kebab-case",
                             "snake_case",
                             "SCREAMING_SNAKE_CASE",
                         },
                         cyclic = true,
-                    },
-                    augend.date.alias["%d/%m/%Y"],
-                    augend.constant.new({ elements = { "and", "or" } }),
-                }
+                    }),
+                },
             })
 
-            vim.keymap.set("n", "<C-a>",  function() require("dial.map").manipulate("increment", "normal") end)
-            vim.keymap.set("n", "<C-x>",  function() require("dial.map").manipulate("decrement", "normal") end)
-            vim.keymap.set("n", "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end)
-            vim.keymap.set("n", "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end)
-            vim.keymap.set("v", "<C-a>",  function() require("dial.map").manipulate("increment", "visual") end)
-            vim.keymap.set("v", "<C-x>",  function() require("dial.map").manipulate("decrement", "visual") end)
-            vim.keymap.set("v", "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end)
-            vim.keymap.set("v", "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end)
+            require("dial.config").augends:on_filetype({
+                rust = extend({
+                    augend.constant.new({
+                        elements = {
+                            "self",
+                            "super",
+                            "crate",
+                        },
+                        cyclic = false,
+                    }),
+                }),
+                cs = extend({
+                    augend.constant.new({
+                        elements = {
+                            "sealed",
+                            "private",
+                            "protected",
+                            "internal",
+                            "public",
+                        },
+                        word = true,
+                        cyclic = true,
+                    }),
+                    augend.constant.new({
+                        elements = {
+                            "abstract",
+                            "virtual",
+                        },
+                        cyclic = true,
+                    }),
+                }),
+                java = extend({
+                    augend.constant.new({
+                        elements = {
+                            "default",
+                            "private",
+                            "protected",
+                            "public",
+                        },
+                        word = true,
+                        cyclic = true,
+                    }),
+                }),
+            })
+
+            local mani = require("dial.map").manipulate
+            vim.keymap.set("n", "<C-a>",  function() mani("increment", "normal") end)
+            vim.keymap.set("n", "<C-x>",  function() mani("decrement", "normal") end)
+            vim.keymap.set("n", "g<C-a>", function() mani("increment", "gnormal") end)
+            vim.keymap.set("n", "g<C-x>", function() mani("decrement", "gnormal") end)
+            vim.keymap.set("v", "<C-a>",  function() mani("increment", "visual") end)
+            vim.keymap.set("v", "<C-x>",  function() mani("decrement", "visual") end)
+            vim.keymap.set("v", "g<C-a>", function() mani("increment", "gvisual") end)
+            vim.keymap.set("v", "g<C-x>", function() mani("decrement", "gvisual") end)
+            vim.keymap.set("n", "<c-g>",  function() mani("increment", "normal", "case") end)
         end,
     })
 end)
@@ -126,9 +174,9 @@ later(function()
                         next = "<m-n>",
                         prev = "<m-p>",
                         dismiss = "<m-e>",
-                    }
-                }
+                    },
+                },
             })
-        end
+        end,
     })
 end)
