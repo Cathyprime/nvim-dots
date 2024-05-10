83b386ba5bcab968021316709e647fdeac8b9e83
Cathyprime <magdajaroszynska47@gmail.com>
2024-05-10 13:35:06 +0200

    fix(none): eslint_d -> eslint_lsp

--- lua/cathy/plugin/nonels.lua
+++ lua/cathy/plugin/nonels.lua
@@ -1,5 +1,3 @@
-require("mini.deps").add("nvimtools/none-ls.nvim")
-
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "BufNewFile" }, {
     once = true,
     callback = function()
@@ -11,11 +9,9 @@ vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "BufNewFile" }, {
                 null_ls.builtins.formatting.gofumpt,
                 null_ls.builtins.formatting.prettier,
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.black,
 
                 null_ls.builtins.diagnostics.golangci_lint,
                 null_ls.builtins.diagnostics.stylelint,
-                require("none-ls.diagnostics.eslint_d"),
             },
         })
         vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
