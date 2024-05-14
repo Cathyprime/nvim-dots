eacbea0e50c103cf8c30727f143f8ed2b6673aa4
Cathyprime <magdajaroszynska47@gmail.com>
2024-05-14 21:20:51 +0200

    feat: nonels: rocks -> deps

--- lua/cathy/plugin/nonels.lua
+++ lua/cathy/plugin/nonels.lua
@@ -1,7 +1,8 @@
+require("mini.deps").add("nvimtools/none-ls.nvim")
+
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "BufNewFile" }, {
     once = true,
     callback = function()
-        vim.cmd.packadd("none-ls-extras.nvim")
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
