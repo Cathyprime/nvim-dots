6953013f7f1775deba2f8e6b1c9555a6c6e087d0
Cathyprime <magdajaroszynska47@gmail.com>
2024-05-28 08:25:01 +0200

    remove: project setup

--- .nvim.lua
+++
@@ -1,8 +0,0 @@
-require("project").registers({
-    a = ":e plugin/autocmd.lua\n",
-    c = ":e plugin/commands.lua\n",
-    k = ":e plugin/keymaps.lua\n",
-    m = ":e lua/cathy/plugin/misc.lua\n",
-    o = ":e lua/cathy/config/options.lua\n",
-    t = ":e lua/cathy/plugin/telescope.lua\n"
-})
