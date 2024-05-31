37049e0da3ccd0c53a6aeb7c2c08559633d8acb1
Cathyprime <magdajaroszynska47@gmail.com>
2024-05-31 17:41:42 +0200

    refactor(editing): move dial to another file

---
+++ lua/cathy/config/dial.lua
@@ -0,0 +1,78 @@
+local augend = require("dial.augend")
+
+local function words(elements, word, cyclic, case)
+    if word == nil then
+        word = true
+    end
+    if cyclic == nil then
+        cyclic = true
+    end
+    if case == nil then
+        case = true
+    end
+    return augend.constant.new({
+        elements = elements,
+        word = word,
+        cyclic = cyclic,
+        preserve_case = case
+    })
+end
+
+local function extend(t)
+    local defaults = {
+        augend.integer.alias.binary,
+        augend.integer.alias.decimal_int,
+        augend.integer.alias.hex,
+        augend.integer.new {
+            radix = 16,
+            prefix = "#",
+            natural = true,
+            case = "upper"
+        },
+        augend.date.alias["%d/%m/%Y"],
+        words({ "&&", "||" }, false, true),
+        words({ "and", "or" }),
+        words({ "true", "false" }),
+        augend.decimal_fraction.new({
+            signed = true,
+            point_char = ".",
+        }),
+        augend.constant.alias.Alpha,
+    }
+    for _, v in ipairs(t or {}) do
+        table.insert(defaults, v)
+    end
+    return defaults
+end
+
+return {
+    register_group = {
+        default = extend({
+            words({ "privacy", "protected", "public" }),
+        }),
+        case = {
+            augend.case.new({
+                types = {
+                    "camelCase",
+                    "PascalCase",
+                    "kebab-case",
+                    "snake_case",
+                    "SCREAMING_SNAKE_CASE",
+                },
+                cyclic = true,
+            }),
+        },
+    },
+    on_filetype = {
+        rust = extend({
+            words({ "self", "super", "crate" }, nil, false),
+        }),
+        cs = extend({
+            words({"sealed", "private", "protected", "internal", "public"}),
+            words({"abstract", "virtual"}),
+        }),
+        java = extend({
+            words({"default", "private", "protected", "public"}),
+        }),
+    }
+}
