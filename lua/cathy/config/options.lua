56f09e92866c06ef94c1863af876223f23363fbf
Cathyprime <magdajaroszynska47@gmail.com>
2024-06-30 19:12:01 +0200

    feat(neovide): options

--- lua/cathy/config/options.lua
+++ lua/cathy/config/options.lua
@@ -146,4 +146,5 @@ if vim.g.neovide then
     vim.g.neovide_cursor_trail_size = 0.2
     vim.g.neovide_cursor_animate_command_line = false
     vim.g.neovide_scroll_animation_length = 0.05
+    vim.g.neovide_position_animation_length = 0.05
 end
