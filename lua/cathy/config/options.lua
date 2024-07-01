239281f8561752f591b19837e093204bf04f4f5d
Cathyprime <magdajaroszynska47@gmail.com>
2024-07-01 13:58:00 +0200

    feat(neovide): even faster animations

--- lua/cathy/config/options.lua
+++ lua/cathy/config/options.lua
@@ -142,9 +142,9 @@ if vim.g.neovide then
     vim.g.neovide_cursor_vfx_mode = ""
     vim.g.neovide_fullscreen = true
     vim.g.neovide_floating_shadow = false
-    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_cursor_animation_length = 0.03
     vim.g.neovide_cursor_trail_size = 0.2
     vim.g.neovide_cursor_animate_command_line = false
-    vim.g.neovide_scroll_animation_length = 0.05
-    vim.g.neovide_position_animation_length = 0.05
+    vim.g.neovide_scroll_animation_length = 0.03
+    vim.g.neovide_position_animation_length = 0.03
 end
