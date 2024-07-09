e2926c079a969b3ec85929ebe20cd1e40e217a72
Cathyprime <magdajaroszynska47@gmail.com>
2024-07-09 22:49:54 +0200

    fix(rust): properly load plugin

--- lua/cathy/plugin/rust.lua
+++ lua/cathy/plugin/rust.lua
@@ -1,4 +1,29 @@
 return {
     "mrcjkb/rustaceanvim",
-    ft = "rust"
+    ft = "rust",
+    init = function()
+        vim.g.rustaceanvim = {
+            dap = {
+                disable = true
+            },
+            tools = {
+                float_win_config = {
+                    border = "single",
+                },
+            },
+            server = {
+                on_attach = function(client, bufnr)
+                    require("cathy.config.lsp-funcs").on_attach(client, bufnr, {
+                        code_action = function()
+                            vim.cmd.RustLsp("codeAction")
+                        end,
+                        hover = function()
+                            vim.cmd.RustLsp({ "hover", "range" })
+                            vim.cmd.RustLsp({ "hover", "actions" })
+                        end,
+                    })
+                end,
+            },
+        }
+    end
 }
