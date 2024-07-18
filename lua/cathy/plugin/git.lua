ab08290a45972e42c84dcfa2382fdfa4a270ae4a
Cathyprime <magdajaroszynska47@gmail.com>
2024-07-18 20:21:36 +0200

    remove(git): merge commands, just remember then 4head

--- lua/cathy/plugin/git.lua
+++ lua/cathy/plugin/git.lua
@@ -9,17 +9,5 @@ return {
                 vim.keymap.set("n", "<tab>", "=", { buffer = true, remap = true })
             end,
         })
-        vim.keymap.set("n", "<leader>dct", function()
-            if vim.opt.diff == "diff" then
-                return "<cmd>diffget //3<CR>"
-            end
-            return "<esc>"
-        end, { desc = "Choose Theirs (//3)", expr = true } )
-        vim.keymap.set("n", "<leader>dco", function()
-            if vim.opt.diff == "diff" then
-                return "<cmd>diffget //2<CR>"
-            end
-            return "<esc>"
-        end, { desc = "Choose Ours (//2)", expr = true } )
     end
 }
