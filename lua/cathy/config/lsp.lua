ab60fb7a74cbda74787e114dcfe413b9785baf4c
Cathyprime <magdajaroszynska47@gmail.com>
2024-07-27 14:08:43 +0200

    feat(omnisharp): use omnisharp_extended

--- lua/cathy/config/lsp.lua
+++ lua/cathy/config/lsp.lua
@@ -52,5 +52,15 @@ require("mason-lspconfig").setup({
         rust_analyzer = disabled,
         -- tsserver = lsp_funcs.tsserver,
         lua_ls = lsp_funcs.lua_ls,
+        omnisharp = function()
+            require("lspconfig").omnisharp.setup({
+                handlers = {
+                    ["textDocument/definition"] = require("omnisharp_extended").definition_handler,
+                    ["textDocument/typeDefinition"] = require("omnisharp_extended").type_definition_handler,
+                    ["textDocument/references"] = require("omnisharp_extended").references_handler,
+                    ["textDocument/implementation"] = require("omnisharp_extended").implementation_handler,
+                }
+            })
+        end
     }
 })
