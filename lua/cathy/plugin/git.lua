b69d921eca64d085746aa52aa8037aacc9bdb9d6
Cathyprime <magdajaroszynska47@gmail.com>
2024-08-28 12:19:28 +0200

    fix(git): misc git settings

--- lua/cathy/plugin/git.lua
+++ lua/cathy/plugin/git.lua
@@ -1,18 +1,4 @@
 return {
-    -- {
-    --     "tpope/vim-fugitive",
-    --     dependencies = "tpope/vim-rhubarb",
-    --     config = function()
-    --         vim.keymap.set("n", "ZG", "<cmd>topleft Git<cr>")
-    --         vim.api.nvim_create_autocmd("Filetype", {
-    --             pattern = "fugitive",
-    --             callback = function()
-    --                 vim.keymap.set("n", "<tab>", "=", { buffer = true, remap = true })
-    --             end,
-    --         })
-    --     end,
-    -- },
-    --
     {
         "sindrets/diffview.nvim",
         keys = {
@@ -24,10 +10,6 @@ return {
     {
         "FabijanZulj/blame.nvim",
         config = true,
-        keys = {
-            { "<leader>gb", "<cmd>BlameToggle<cr>" },
-            { "<leader>gB", "<cmd>BlameToggle virtual<cr>" },
-        }
     },
     {
         "NeogitOrg/neogit",
