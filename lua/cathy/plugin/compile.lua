ddddc7fe116e5fd2254e952b748e5095a6810fe6
Cathyprime <magdajaroszynska47@gmail.com>
2024-08-30 00:27:34 +0200

    feat(compile): hog wild on customizing

--- lua/cathy/plugin/compile.lua
+++ lua/cathy/plugin/compile.lua
@@ -1,120 +1,95 @@
-local function start_wrapper()
-    if vim.b["start_compile"] then
-        return string.format("<cmd>Start -wait=always %s<cr>", vim.b["start_compile"])
-    end
-    local ok, c = pcall(vim.fn.input, {
-        prompt = ":Start ",
-        default = vim.b["start_compile"] or "",
-        cancelreturn = -99,
-        completion = "custom,v:lua.CustomFilesystemCompletion"
-    })
-    if c == -99 or not ok then return "" end
-    vim.cmd("redraw")
-    vim.b["start_compile"] = c
-    return string.format("<cmd>Start -wait=always %s<cr>", vim.b["start_compile"])
-end
-
-local function start_wrapper_change()
-    if not vim.b["start_compile"] then
-        return start_wrapper()
-    end
-    local ok, c = pcall(vim.fn.input, {
-        prompt = ":Start ",
-        default = vim.b["start_compile"] or "",
-        cancelreturn = -99,
-        completion = "custom,v:lua.CustomFilesystemCompletion"
-    })
-    if not ok or c == -99 then return "" end
-    vim.cmd("redraw")
-    vim.b["start_compile"] = c
-    return string.format("<cmd>Start -wait=always %s<cr>", c)
-end
-
-local function dispatch_wrapper()
-    vim.g["dispatch_ready"] = true
-    if vim.b["dispatch"] then
-        return "<cmd>Dispatch!<cr>"
-    end
-    local ok, c = pcall(vim.fn.input, {
-        prompt = ":Compile command ",
-        default = vim.b.dispatch or "",
-        cancelreturn = -99,
-        completion = "custom,v:lua.CustomFilesystemCompletion"
-    })
-    if c == -99 or not ok then return "" end
-    vim.cmd("redraw")
-    vim.b["dispatch"] = c
-    return "<cmd>Dispatch!<cr>"
-end
-
-local function dispatch_wrapper_change()
-    vim.g["dispatch_ready"] = true
-    if not vim.b.dispatch then
-        return dispatch_wrapper()
-    end
-    local ok, c = pcall(vim.fn.input, {
-        prompt = ":Compile command ",
-        default = vim.b.dispatch or "",
-        cancelreturn = -99,
-        completion = "custom,v:lua.CustomFilesystemCompletion"
-    })
-    if not ok or c == -99 then return "" end
-    vim.cmd("redraw")
-    vim.b["dispatch"] = c
-    return "<cmd>Dispatch!<cr>"
-end
-
-local function make_wrapper()
-    vim.g["dispatch_ready"] = true
-    if vim.g["make_compile"] then
-        return string.format("<cmd>Make! %s<cr>", vim.g["make_compile"])
-    end
-    local ok, c = pcall(vim.fn.input, {
-        prompt = ":make ",
-        default = vim.g["make_compile"] or "",
-        cancelreturn = -99,
-    })
-    vim.cmd("redraw")
-    if not ok or c == -99 then return "" end
-    vim.g["make_compile"] = c
-    return string.format("<cmd>Make! %s<cr>", c)
-end
-
-local function make_wrapper_change()
-    vim.g["dispatch_ready"] = true
-    if not vim.g["make_compile"] then
-        return make_wrapper()
-    end
-    local ok, c = pcall(vim.fn.input, {
-        prompt = ":make ",
-        default = vim.g["make_compile"] or "",
-        cancelreturn = -99,
-        completion = "custom,v:lua.CustomFilesystemCompletion"
-    })
-    if not ok or c == -99 then return "" end
-    vim.cmd("redraw")
-    vim.g["make_compile"] = c
-    return string.format("<cmd>Make! %s<cr>", c)
-end
-
 vim.g.dispatch_handlers = {
     "terminal",
     "headless",
     "job",
 }
 
+vim.api.nvim_create_autocmd("VimEnter", {
+    once = true,
+    callback = function()
+        vim.api.nvim_del_user_command("Dispatch")
+        vim.api.nvim_create_user_command(
+            "Dispatch",
+            function(opts)
+                local count = 0
+                local args = opts.args or ""
+                local mods = opts.mods or ""
+                local bang = opts.bang and 1 or 0
+
+                if bang == 1 then
+                    vim.g["dispatch_ready"] = true
+                end
+                if opts.count < 0 or opts.line1 == opts.line2 then
+                    count = opts.count
+                end
+                if args == "" and vim.b.dispatch ~= "" then
+                    args = vim.b.dispatch
+                end
+                vim.b["dispatch"] = args
+                vim.fn["dispatch#compile_command"](bang, args, count, mods)
+            end,
+            {
+                bang = true,
+                nargs = "*",
+                range = -1,
+                complete = "customlist,dispatch#command_complete",
+            }
+        )
+        vim.api.nvim_del_user_command("Start")
+        vim.api.nvim_create_user_command(
+            "Start",
+            function(opts)
+                local count = 0
+                local args = opts.args or ""
+                local mods = opts.mods or ""
+                local bang = opts.bang and 1 or 0
+
+                if opts.count < 0 or opts.line1 == opts.line2 then
+                    count = opts.count
+                end
+                if args == "" and vim.b.start_compile ~= "" then
+                    args = vim.b.start_compile or ""
+                end
+                vim.b["start_compile"] = args
+                vim.fn["dispatch#start_command"](bang, "-wait=always "..args, count, mods)
+            end,
+            {
+                bang = true,
+                nargs = "*",
+                range = -1,
+                complete = "customlist,dispatch#command_complete",
+            }
+        )
+        vim.api.nvim_del_user_command("Copen")
+        vim.api.nvim_create_user_command(
+            "Copen",
+            function(opts)
+                local bang = opts.bang and 1 or 0
+                vim.g["dispatch_ready"] = false
+                vim.fn["dispatch#copen"](bang, opts.mods or "")
+            end,
+            {
+                bang = true,
+                bar = true,
+            }
+        )
+    end,
+})
+
 return {
-    "tpope/vim-dispatch",
-    config = function()
-        vim.keymap.set("n", "Zc",        "<cmd>AbortDispatch<cr>", { silent = true               })
-        vim.keymap.set("n", "ZC",        "<cmd>AbortDispatch<cr>", { silent = true               })
-        vim.keymap.set("n", "ZF",        "<cmd>Focus!<cr>",        { silent = true               })
-        vim.keymap.set("n", "Zf",        ":Focus ",                { silent = false              })
-        vim.keymap.set("n", "ZS",        start_wrapper_change,     { silent = false, expr = true })
-        vim.keymap.set("n", "Zs",        start_wrapper,            { silent = false, expr = true })
-        vim.keymap.set("n", "ZD",        dispatch_wrapper_change,  { silent = false, expr = true })
-        vim.keymap.set("n", "Zd",        dispatch_wrapper,         { silent = false, expr = true })
-        vim.keymap.set("n", "Zm",        make_wrapper,             { silent = false, expr = true })
-        vim.keymap.set("n", "ZM",        make_wrapper_change,      { silent = false, expr = true })
-    end
+    {
+        "tpope/vim-dispatch",
+        config = function()
+            vim.keymap.set("n", "Zc", "<cmd>AbortDispatch<cr>", { silent = true  })
+            vim.keymap.set("n", "ZC", "<cmd>AbortDispatch<cr>", { silent = true  })
+            vim.keymap.set("n", "ZF", "<cmd>Focus!<cr>",        { silent = true  })
+            vim.keymap.set("n", "Zf", ":Focus ",                { silent = false })
+            vim.keymap.set("n", "Zm", "<cmd>Make<cr>",          { silent = true  })
+            vim.keymap.set("n", "ZM", "<cmd>Make<cr>",          { silent = true  })
+            vim.keymap.set("n", "Zd", "<cmd>Dispatch<cr>",      { silent = true  })
+            vim.keymap.set("n", "ZD", ":Dispatch ",             { silent = false })
+            vim.keymap.set("n", "ZS", ":Start ",                { silent = false })
+            vim.keymap.set("n", "Zs", "<cmd>Start<cr>",         { silent = true  })
+        end,
+    },
 }
