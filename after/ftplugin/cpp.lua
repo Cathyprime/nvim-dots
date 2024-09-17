434beb841985161c468ef7cba9c57bac2a14bdcd
Cathyprime <magdajaroszynska47@gmail.com>
2024-09-17 02:10:20 +0200

    feat(options): indentation settings

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,10 +1,3 @@
-vim.api.nvim_create_autocmd("BufEnter", {
-    once = true,
-    callback = function()
-        vim.opt.indentkeys:remove(":")
-    end
-})
-
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
