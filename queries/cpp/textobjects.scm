b9085247e13c15beae8a9a0d12b3e2f54344bba1
Cathyprime <magdajaroszynska47@gmail.com>
2024-09-19 00:15:22 +0200

    feat(options): return type indent

--- queries/cpp/textobjects.scm
+++ queries/cpp/textobjects.scm
@@ -1,3 +1,9 @@
 (qualified_identifier
  name: (template_type
          arguments: (_) @type.inner )) @type.outer
+
+(qualified_identifier
+  name: (qualified_identifier
+          scope: (template_type
+                   arguments: (_) @template.inner)
+          )) @template.outer
