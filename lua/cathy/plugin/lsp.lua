396fa52355d5472a7090af2feb6f406dd5f7a03a
Cathyprime <magdajaroszynska47@gmail.com>
2024-09-20 23:18:34 +0200

    feat(lsp): fidget -> lspecho

--- lua/cathy/plugin/lsp.lua
+++ lua/cathy/plugin/lsp.lua
@@ -3,26 +3,11 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "neovim/nvim-lspconfig",
-        "j-hui/fidget.nvim",
+        "deathbeam/lspecho.nvim",
     },
     config =  function()
         require("cathy.config.lsp")
-        require("fidget").setup({
-            progress = {
-                display = {
-                    progress_icon = {
-                        pattern = "moon",
-                        period = 1,
-                    },
-                },
-            },
-            notification = {
-                window = {
-                    winblend = 0,
-                    max_width = 90,
-                },
-            }
-        })
+        require("lspecho").setup()
     end,
     event = "VeryLazy",
 }
