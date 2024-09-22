143a08acab5512cb9bbfc4d96f81ce0dda0e56d3
Cathyprime <magdajaroszynska47@gmail.com>
2024-09-22 03:00:37 +0200

    fix(treesitter): cpp hightlighs

--- queries/cpp/highlights.scm
+++ queries/cpp/highlights.scm
@@ -2,10 +2,3 @@
 
 (preproc_def
   name: (identifier) @constant.macro)
-
-(preproc_call
-  directive: (preproc_directive)
-  argument: (preproc_arg
-              (translation_unit
-                (expression_statement
-                  (identifier) @function.macro))))
