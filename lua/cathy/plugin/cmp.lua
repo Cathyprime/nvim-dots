cb9d8aa2de61169dca7ec5e93c2a61339db60020
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-06 21:52:35 +0200

    feat(cmp): use a fork of cmp

--- lua/cathy/plugin/cmp.lua
+++ lua/cathy/plugin/cmp.lua
@@ -11,7 +11,8 @@ return {
         },
     },
     {
-        "hrsh7th/nvim-cmp",
+        "iguanacucumber/magazine.nvim",
+        name = "nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
             "L3MON4D3/LuaSnip",
