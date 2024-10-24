e2ccbc156bc834ae083d6f2a1dc9f2a82315ba34
Cathyprime <magdajaroszynska47@gmail.com>
2024-10-24 21:35:20 +0200

    fix(go): don't set errors

--- lua/cathy/plugin/go.lua
+++ lua/cathy/plugin/go.lua
@@ -4,5 +4,12 @@ return {
     opts = {
         dap_debug = true,
         lsp_codelens = false,
+        diagnostic = {
+            hdlr = false,
+            underline = false,
+            virtual_text = false,
+            signs = false,
+            update_in_insert = false
+        }
     },
 }
